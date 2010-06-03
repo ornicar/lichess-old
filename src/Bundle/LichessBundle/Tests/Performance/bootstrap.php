30e6345f49665353ed947ae89af5f46c3416e709
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-04 01:36:51 +0200

    Add unit tests and performance tests

---
+++ src/Bundle/LichessBundle/Tests/Performance/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(__DIR__.'/../../Entities/Game.php');
+require_once(__DIR__.'/../../Entities/Player.php');
+require_once(__DIR__.'/../../Entities/Piece.php');
+require_once(__DIR__.'/../../Entities/Piece/Pawn.php');
+require_once(__DIR__.'/../../Entities/Piece/Rook.php');
+require_once(__DIR__.'/../../Entities/Piece/Knight.php');
+require_once(__DIR__.'/../../Entities/Piece/Bishop.php');
+require_once(__DIR__.'/../../Entities/Piece/Queen.php');
+require_once(__DIR__.'/../../Entities/Piece/King.php');
+
+require_once(__DIR__.'/../../Chess/Generator.php');
