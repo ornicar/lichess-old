77ed7cf67d397111d5973dfa7bd846b8db4a1836
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-04 21:59:51 +0200

    Add Board with unit tests

---
+++ src/Bundle/LichessBundle/Chess/Board.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Bundle\LichessBundle\Chess;
+
+use Bundle\LichessBundle\Entities\Game;
+
+class Board
+{
+  protected
+  $game,
+  $squares,
+  $cache;
+  
+  public function __construct(Game $game)
+  {
+    $this->game = $game;
+    $this->createSquares();
+    $this->compile();
+  }
+  
+  public function compile()
+  {
+      $this->cache = array_fill(1, 8, array_fill(1, 8, null));
+
+      foreach($this->getPieces() as $piece)
+      {
+          $this->cache[$piece->getSquareKey()] = $piece;
+      }
+  }
+  
+  public function getGame()
+  {
+    return $this->game;
+  }
+  
+  public function getPlayers()
+  {
+    return $this->game->getPlayers();
+  }
+  
+  public function getPieces()
+  {
+    return $this->game->getPieces();
+  }
+  
+  public function getSquares()
+  {
+    return $this->squares;
+  }
+  
+  public function getSquareKeys()
+  {
+    return array_keys($this->squares);
+  }
+
+  public function getSquareByKey($key)
+  {
+    return isset($this->squares[$key]) ? $this->squares[$key] : null;
+  }
+  
+  public function getSquareByPos($x, $y)
+  {
+    return $this->getSquareByKey('s'.$x.$y);
+  }
+  
+  public function getPieceByKey($key)
+  {
+      return $this->cache[$key];
+  }
+  
+  public function getPieceByPos($x, $y)
+  {
+    return $this->getPieceByKey('s'.$x.$y);
+  }
+  public function getPieceByHumanPos($humanPos)
+  {
+    return $this->getPieceByKey($this->humanPosToKey($humanPos));
+  }
+  
+  protected function createSquares()
+  {
+    $this->squares = array();
+    
+    for($x=1; $x<9; $x++)
+    {
+      for($y=1; $y<9; $y++)
+      {
+          $key = 's'.$x.$y;
+          $color = ($x+$y)%2 ? 'white' : 'black';
+          $this->squares[$key] = new Square($this, $key, $x, $y, $color);
+      }
+    }
+  }
+
+  public function squaresToKeys(array $squares)
+  {
+    $keys = array();
+    foreach($squares as $square)
+    {
+      $keys[] = $square->getKey();
+    }
+    return $keys;
+  }
+
+  /**
+   * removes non existing or duplicated square
+   */
+  public function cleanSquares(array $squares, $passedKeys = array())
+  {
+    foreach($squares as $it => $square)
+    {
+      if($square instanceof Square)
+      {
+        $key = $square->getKey();
+      }
+      else
+      {
+        unset($squares[$it]);
+        continue;
+      }
+      
+      if(in_array($key, $passedKeys))
+      {
+        unset($squares[$it]);
+      }
+      else
+      {
+        $passedKeys[] = $key;
+      }
+    }
+    
+    return array_values($squares);
+  }
+  
+  public function humanPosToKey($pos)
+  {
+    if(is_array($pos))
+    {
+      foreach($pos as $i => $p)
+      {
+        $pos[$i] = $this->humanPosToKey($p);
+      }
+      return $pos;
+    }
+    
+    $letters = 'abcdefgh';
+    return 's'.(1+strpos($letters, $pos{0})).$pos{1};
+  }
+  
+  public function keyToHumanPos($key)
+  {
+    if(is_array($key))
+    {
+      foreach($key as $i => $k)
+      {
+        $key[$i] = $this->keyToHumanPos($k);
+      }
+      return $key;
+    }
+    
+    $letters = 'abcdefgh';
+    return $letters{$key{1}-1}.$key{2};
+  }
+}
---
+++ src/Bundle/LichessBundle/Tests/Chess/BoardTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Bundle\LichessBundle\Tests\Chess;
+
+use Bundle\LichessBundle\Chess\Board;
+use Bundle\LichessBundle\Chess\Square;
+use Bundle\LichessBundle\Chess\Generator;
+use Bundle\LichessBundle\Entities as Entities;
+
+require_once __DIR__.'/../../Entities/Game.php';
+require_once __DIR__.'/../../Entities/Player.php';
+require_once __DIR__.'/../../Entities/Piece.php';
+require_once __DIR__.'/../../Entities/Piece/Pawn.php';
+require_once __DIR__.'/../../Entities/Piece/Rook.php';
+require_once __DIR__.'/../../Entities/Piece/Knight.php';
+require_once __DIR__.'/../../Entities/Piece/Bishop.php';
+require_once __DIR__.'/../../Entities/Piece/Queen.php';
+require_once __DIR__.'/../../Entities/Piece/King.php';
+require_once __DIR__.'/../../Chess/Generator.php';
+require_once __DIR__.'/../../Chess/Board.php';
+require_once __DIR__.'/../../Chess/Square.php';
+
+class BoardTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testBoardCreation()
+    {
+        $generator = new Generator();
+        $game = $generator->createGame();
+        $board = $game->getBoard();
+
+        $this->assertTrue($board instanceof Board);
+
+        return $board;
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testGetGame(Board $board)
+    {
+        $this->assertTrue($board->getGame() instanceof Entities\Game);
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testGetSquares(Board $board)
+    {
+        $squares = $board->getSquares();
+        $this->assertTrue(is_array($squares));
+        $this->assertEquals(64, count($squares));
+        $square = array_shift($squares);
+        $this->assertTrue($square instanceof Square);
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testGetSquareByKey(Board $board)
+    {
+        $key = 's11';
+        $square = $board->getSquareBykey($key);
+        $this->assertTrue($square instanceof Square);
+        $this->assertEquals('black', $square->getColor());
+        $this->assertEquals(1, $square->getX());
+        $this->assertEquals(1, $square->getY());
+        $this->assertEquals($key, $square->getKey());
+
+        $this->assertTrue($board->getSquareByKey('s88') instanceof Square);
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testGetSquareByPos(Board $board)
+    {
+        $square = $board->getSquareByPos(1, 1);
+        $this->assertSame($board->getSquareByKey('s11'), $square);
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testGetPieceByKey(Board $board)
+    {
+        $piece = $board->getPieceByKey('s11');
+        $this->assertEquals('Rook', $piece->getClass());
+        $this->assertEquals(1, $piece->getX());
+        $this->assertEquals(1, $piece->getY());
+        $this->assertEquals('white', $piece->getColor());
+        $this->assertSame($board, $piece->getBoard());
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testGetPieceByPos(Board $board)
+    {
+        $this->assertSame($board->getPieceByKey('s11'), $board->getPieceByPos(1, 1));
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testSquaresToKeys(Board $board)
+    {
+        $squares = array(
+            $board->getSquareByPos(1, 1),
+            $board->getSquareByPos(3, 8)
+        );
+
+        $this->assertSame(array('s11', 's38'), $board->squaresToKeys($squares));
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testCleanSquares(Board $board)
+    {
+        $squares = array(
+            $board->getSquareByPos(1, 1),
+            null,
+            $board->getSquareByPos(3, 8),
+            $board->getSquareByPos(3, 8)
+        );
+        $cleanSquares = array(
+            $board->getSquareByPos(1, 1),
+            $board->getSquareByPos(3, 8)
+        );
+        $this->assertSame($cleanSquares, $board->cleanSquares($squares));
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testKeyToHumanPos(Board $board)
+    {
+        $this->assertEquals('a1', $board->keyToHumanPos('s11'));
+        $this->assertEquals('h8', $board->keyToHumanPos('s88'));
+        $this->assertEquals('b4', $board->keyToHumanPos('s24'));
+    }
+
+    /**
+     * @depends testBoardCreation
+     */
+    public function testHumanPosToKey(Board $board)
+    {
+        $this->assertEquals('s11', $board->humanPosToKey('a1'));
+        $this->assertEquals('s88', $board->humanPosToKey('h8'));
+        $this->assertEquals('s24', $board->humanPosToKey('b4'));
+    }
+}
