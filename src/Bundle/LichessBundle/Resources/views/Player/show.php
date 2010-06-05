c37473c9054b936f87be96be0b087f4b1aed0dc7
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-05 03:05:40 +0200

    Add routing and views for gameplay

---
+++ src/Bundle/LichessBundle/Resources/views/Player/show.php
@@ -0,0 +1,11 @@
+<?php $view->extend('LichessBundle::layout') ?>
+
+<div class="lichess_game clearfix lichess_player_<?php echo $player->getColor() ?>">
+    <div class="lichess_board_wrap">
+        <?php $view->output('LichessBundle:Game:board', array('player' => $player)) ?>
+    </div> 
+    <div class="lichess_table_wrap">
+        <div class="lichess_table">
+        </div>
+    </div>
+</div>
