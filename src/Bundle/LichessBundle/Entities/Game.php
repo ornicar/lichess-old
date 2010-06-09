a8e77f1a686ebdab6c1ee9660ed5cec6461aecae
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-10 00:38:59 +0200

    Remove Entity cache

--- src/Bundle/LichessBundle/Entities/Game.php
+++ src/Bundle/LichessBundle/Entities/Game.php
@@ -287,13 +287,6 @@ class Game
         return $clone;
     }
 
-    public function clearCache()
-    {
-        foreach($this->players as $player) {
-            $player->clearCache();
-        }
-    }
-
     public function __clone()
     {
     }
