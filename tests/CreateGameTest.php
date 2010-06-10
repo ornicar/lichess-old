b930af29a5d4970054c9b540ba65ba03be14d2f0
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-11 00:13:01 +0200

    Add functional test skeleton

---
+++ tests/CreateGameTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lichess\Tests;
+
+use Symfony\Framework\WebBundle\Test\WebTestCase;
+
+class CreateGameTest extends WebTestCase
+{
+    public function testCreation()
+    {
+        $this->assertTrue(true);
+    }
+}
