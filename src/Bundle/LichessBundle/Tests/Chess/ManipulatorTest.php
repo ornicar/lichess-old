4f71de040cf487fae283f654f981c22f15b001e4
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-11 18:30:27 +0200

    Remove useless use statement

--- src/Bundle/LichessBundle/Tests/Chess/ManipulatorTest.php
+++ src/Bundle/LichessBundle/Tests/Chess/ManipulatorTest.php
@@ -6,7 +6,6 @@ use Bundle\LichessBundle\Chess\Generator;
 use Bundle\LichessBundle\Chess\Manipulator;
 
 require_once __DIR__.'/../gameBootstrap.php';
-require_once __DIR__.'/../../Chess/Manipulator.php';
 
 class ManipulatorTest extends \PHPUnit_Framework_TestCase
 {
