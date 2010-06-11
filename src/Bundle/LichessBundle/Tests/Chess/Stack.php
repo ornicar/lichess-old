64010e8257e46fef5730031d607db41411fdb344
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-11 18:28:04 +0200

    Introduce event stack

---
+++ src/Bundle/LichessBundle/Tests/Chess/Stack.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bundle\LichessBundle\Tests\Chess;
+
+use Bundle\LichessBundle\Chess\Generator;
+use Bundle\LichessBundle\Chess\Manipulator;
+use Bundle\LichessBundle\Chess\Stack;
+
+require_once __DIR__.'/../gameBootstrap.php';
+
+class StackTest extends \PHPUnit_Framework_TestCase
+{
+    protected $game;
+
+    public function test3()
+    {
+        $data = <<<EOF
+r  q rk 
+pp  ppbp
+ np  np 
+  Q   B 
+   pp b 
+  N  N  
+PP   PPP
+R   KB R
+EOF;
+        $stack = new Stack();
+        $manipulator = new Manipulator($this->game->getBoard(), $stack);
+        $this->assertEquals(array(), $stack->getEvents());
+        $manipulator->play('a2 a4');
+        $this->assertEquals(array(array('type' => 'move', 'from' => 'a2', 'to' => 'a4')));
+    }
+
+    /**
+     * Get a game from visual data block
+     *
+     * @return Game
+     **/
+    protected function createGame($data)
+    {
+        $generator = new Generator();
+        $this->game = $generator->createGameFromVisualBlock($data);
+        return $this->game; 
+    }
+}
