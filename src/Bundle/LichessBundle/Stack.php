4a566bf706569444f8653cf096f4ea3f6b113203
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-11 18:28:04 +0200

    Introduce event stack

---
+++ src/Bundle/LichessBundle/Stack.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bundle\LichessBundle;
+
+class Stack
+{
+    /**
+     * Events in the stack
+     *
+     * @var array
+     */
+    protected $events = array();
+    
+    /**
+     * Get events
+     * @return array
+     */
+    public function getEvents()
+    {
+      return $this->events;
+    }
+    
+    /**
+     * Set events
+     * @param  array
+     * @return null
+     */
+    public function setEvents($events)
+    {
+      $this->events = $events;
+    }
+
+    public function add(array $event)
+    {
+    }
+}
