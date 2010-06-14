2bcf242f5da921ef2107abefa2dd47672422b051
ornicar <thibault.duplessis@gmail.com>
2010-06-14 10:34:13 +0200

    Reduce usage of class loader

--- src/Bundle/LichessBundle/Bundle.php
+++ src/Bundle/LichessBundle/Bundle.php
@@ -8,6 +8,29 @@ use Symfony\Components\DependencyInjection\ContainerInterface;
 use Symfony\Components\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Components\DependencyInjection\BuilderConfiguration;
 
+/**
+ * Reduce usage of class loader for performance reasons
+ */
+require_once __DIR__.'/Socket.php';
+require_once __DIR__.'/Stack.php';
+require_once __DIR__.'/Chess/Square.php';
+require_once __DIR__.'/Chess/Board.php';
+require_once __DIR__.'/Chess/Analyser.php';
+require_once __DIR__.'/Chess/Manipulator.php';
+require_once __DIR__.'/Chess/PieceFilter.php';
+require_once __DIR__.'/Entities/Piece.php';
+require_once __DIR__.'/Entities/Piece/Bishop.php';
+require_once __DIR__.'/Entities/Piece/King.php';
+require_once __DIR__.'/Entities/Piece/Knight.php';
+require_once __DIR__.'/Entities/Piece/Pawn.php';
+require_once __DIR__.'/Entities/Piece/Queen.php';
+require_once __DIR__.'/Entities/Piece/Rook.php';
+require_once __DIR__.'/Entities/Player.php';
+require_once __DIR__.'/Entities/Game.php';
+require_once __DIR__.'/Persistence/PersistenceInterface.php';
+require_once __DIR__.'/Persistence/FilePersistence.php';
+require_once __DIR__.'/Logger/BlackHole.php';
+
 class Bundle extends BaseBundle
 {
     public function buildContainer(ContainerInterface $container)
