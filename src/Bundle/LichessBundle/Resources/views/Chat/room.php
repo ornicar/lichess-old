9bea1dd70530fcd47170d45708a0ff8695a8f11d
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-17 23:10:47 +0200

    Remove unused image

--- src/Bundle/LichessBundle/Resources/views/Chat/room.php
+++ src/Bundle/LichessBundle/Resources/views/Chat/room.php
@@ -1,7 +1,7 @@
 <div class="lichess_chat">
     <ol class="lichess_messages">
     <?php foreach($player->getGame()->getRoom()->getMessages() as $message): ?>
-        <li><em><?php echo $message[0] ?></em><?php echo htmlentities($message[1], ENT_COMPAT, 'UTF-8') ?></li>
+        <li><em class="<?php echo $message[0] ?>"></em><?php echo htmlentities($message[1], ENT_COMPAT, 'UTF-8') ?></li>
     <?php endforeach; ?>
     </ol>
     <form action="<?php echo $view->router->generate('lichess_say', array('hash' => $player->getFullHash())) ?>" method="POST">
