47a38c92f26b41ada1499344dbba8766a69fdc95
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-19 17:16:47 +0200

    Start implementing chess roulette

--- src/Bundle/LichessBundle/Resources/views/Main/index.php
+++ src/Bundle/LichessBundle/Resources/views/Main/index.php
@@ -7,14 +7,13 @@
     </div> 
     <div class="lichess_table_wrap">
         <div class="lichess_table lichess_table_not_started">
-            <a class="lichess_button lichess_toggle_join_url">Play with a friend</a>
+            <a class="lichess_button lichess_toggle_join_url" title="Invite a friend to play with you">Play with a friend</a>
             <div class="lichess_join_url">
                 <p>To invite someone to play, give this url:</p>
                 <span><?php echo $view->router->generate('lichess_game', array('hash' => $player->getGame()->getHash()), true) ?></span>
             </div>
-            <div class="lichess_join_ai">
-                <a href="<?php echo $view->router->generate('lichess_invite_ai', array('hash' => $player->getFullHash())) ?>" class="lichess_button">Play with the machine</a>
-            </div>
+            <a href="<?php echo $view->router->generate('lichess_invite_ai', array('hash' => $player->getFullHash())) ?>" class="lichess_button" title="Challenge the artificial intelligence">Play with the machine</a>
+            <a href="<?php echo $view->router->generate('lichess_play_with_anybody', array('hash' => $player->getFullHash())) ?>" class="lichess_button" title="Pick a random human opponent">Play with anybody<span class="lichess_new">new</span></a>
         </div>
     </div>
 </div>
