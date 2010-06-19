7d6332a3a096c6ea2c4af057e04f1bdf9b97bd90
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-19 13:12:49 +0200

    Prevent munin from creating new games

--- src/Bundle/LichessBundle/Controller/MainController.php
+++ src/Bundle/LichessBundle/Controller/MainController.php
@@ -11,18 +11,19 @@ class MainController extends Controller
 
     public function indexAction($color)
     {
-        // When munin pings the website, don't create a new game
-        if(isset($_SERVER['HTTP_USER_AGENT']) && 0 === strncmp($_SERVER['HTTP_USER_AGENT'], 'Wget/', 5)) {
-            return $this->createResponse('alive');
-        }
 
         $game = $this->getNewGame();
         $player = $game->getPlayer($color);
         $game->setCreator($player);
 
-        $this->container->getLichessPersistenceService()->save($game);
-        $socket = new Socket($player, $this->container['kernel.root_dir'].'/cache/socket');
-        $socket->write(array());
+        if(isset($_SERVER['REMOTE_ADDR']) && '127.0.0.1' == $_SERVER['REMOTE_ADDR']) {
+            // When munin pings the website, don't save the new game
+        }
+        else {
+            $this->container->getLichessPersistenceService()->save($game);
+            $socket = new Socket($player, $this->container['kernel.root_dir'].'/cache/socket');
+            $socket->write(array());
+        }
 
         return $this->render('LichessBundle:Main:index', array(
             'player' => $player
