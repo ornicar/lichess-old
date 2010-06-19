231a4d084d58ae07a0a4700166476b3888c6f9aa
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-19 14:28:44 +0200

    Add unit test for en passant

--- src/Bundle/LichessBundle/Tests/Chess/EnPassantTest.php
+++ src/Bundle/LichessBundle/Tests/Chess/EnPassantTest.php
@@ -4,6 +4,7 @@ namespace Bundle\LichessBundle\Tests\Chess;
 
 use Bundle\LichessBundle\Chess\Generator;
 use Bundle\LichessBundle\Chess\Manipulator;
+use Bundle\LichessBundle\Chess\Analyser;
 use Bundle\LichessBundle\Chess\PieceFilter;
 
 require_once __DIR__.'/../gameBootstrap.php';
@@ -25,12 +26,32 @@ pP
         
 K       
 EOF;
-        $game = $this->game = $this->createGame($data);
+        $this->createGame($data);
         $this->game->getBoard()->getPieceByKey('a5')->setFirstMove($this->game->getTurns()-1);
+        $this->game->getBoard()->getPieceByKey('b5')->setFirstMove(0);
+        $this->assertMoves('b5', 'b6 a6');
         $this->move('b5 a6');
         $this->assertTrue($this->game->getBoard()->getPieceByKey('a6')->isClass('Pawn'));
         $this->assertTrue($this->game->getBoard()->getSquareByKey('b5')->isEmpty());
         $this->assertTrue($this->game->getBoard()->getSquareByKey('a5')->isEmpty());
+    }
+
+    public function testEnPassantImpossibleWhenOneSquareOnly()
+    {
+        $data = <<<EOF
+       k
+        
+pP      
+        
+        
+        
+        
+K       
+EOF;
+        $this->createGame($data);
+        $this->game->getBoard()->getPieceByKey('a6')->setFirstMove($this->game->getTurns()-1);
+        $this->game->getBoard()->getPieceByKey('b6')->setFirstMove(0);
+        $this->assertMoves('b6', 'b7');
     }
 
     /**
@@ -51,17 +72,25 @@ EOF;
      *
      * @return Game
      **/
-    protected function createGame($data = null)
+    protected function createGame($data, $blackTurn = false)
     {
         $generator = new Generator();
-        if ($data) {
-            $game = $generator->createGameFromVisualBlock($data);
-        }
-        else {
-            $game = $generator->createGame();
-        }
-        $game->setIsStarted(true);
-        $game->setTurns(30);
-        return $game; 
+        $this->game = $generator->createGameFromVisualBlock($data);
+        $this->game->setIsStarted(true);
+        $this->game->setTurns($blackTurn ? 11 : 10);
+        $this->analyser = new Analyser($this->game->getBoard());
+    }
+
+    protected function assertMoves($pieceKey, $moves)
+    {
+        $moves = explode(' ', $moves);
+        $possibleMoves = $this->analyser->getPlayerPossibleMoves($this->game->getTurnPlayer());
+        $this->assertEquals($this->sort($moves), isset($possibleMoves[$pieceKey]) ? $this->sort($possibleMoves[$pieceKey]) : null);
+    }
+
+    protected function sort($array)
+    {
+        sort($array);
+        return $array;
     }
 }
