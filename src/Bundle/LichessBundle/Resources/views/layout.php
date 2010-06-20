185ad053873c3d84d2b862bab0e302d53398bdb0
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-20 15:32:43 +0200

    Allow to disable move hints

--- src/Bundle/LichessBundle/Resources/views/layout.php
+++ src/Bundle/LichessBundle/Resources/views/layout.php
@@ -40,18 +40,7 @@
             </div>
         </div>
         <script src="/bundle/lichess/js/lib.min.js" type="text/javascript"></script>
-        <script src="/bundle/lichess/js/ctrl.js" type="text/javascript"></script>
-        <script src="/bundle/lichess/js/game.1.js" type="text/javascript"></script>
-        <script type="text/javascript">
-if(document.domain == 'lichess.org') {
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-7935029-3']);
-_gaq.push(['_trackPageview']);
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = 'http://www.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-}
-        </script>
+        <script src="/bundle/lichess/js/ctrl.1.js" type="text/javascript"></script>
+        <script src="/bundle/lichess/js/game.2.js" type="text/javascript"></script>
     </body>
 </html>
