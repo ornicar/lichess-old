9faeef7875c0e430fd4e43a46a521492b65879ab
ornicar <thibault.duplessis@gmail.com>
2010-06-22 10:48:56 +0200

    Css tweaks

--- src/Bundle/LichessBundle/Resources/views/layout.php
+++ src/Bundle/LichessBundle/Resources/views/layout.php
@@ -34,7 +34,9 @@
                     Contact: <span class="js_email"></span><br />
                     <a href="<?php echo $view->router->generate('lichess_about') ?>" target="_blank">Learn more about Lichess</a>
                 </div>
-                    <a class="lichess_uservoice">Feedback</a> / <a href="http://github.com/ornicar/lichess" target="_blank">Source code</a><br />
+                <a class="lichess_uservoice" title="Having a suggestion, feature request or bug report? Let me know">Feedback</a> /
+                <a href="http://www.pledgie.com/campaigns/11351" target="_blank" title="Lichess is free, support it!">Donate</a> /
+                <a href="http://github.com/ornicar/lichess" target="_blank" title="See what's inside, fork and contribute">Source code</a><br />
                 Open Source software built with PHP 5.3, <a href="http://symfony-reloaded.org" target="_blank">Symfony 2</a> &amp; <a href="http://jqueryui.com/" target="_blank">jQuery UI</a><br />
                 Artificial intelligence: <a href="http://www.craftychess.com/" target="_blank">Crafty</a>
             </div>
