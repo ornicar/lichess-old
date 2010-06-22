aa264d100935a5f84c63419eda09749d4ab2a5c0
ornicar <thibault.duplessis@gmail.com>
2010-06-22 10:55:40 +0200

    Fix ga

--- src/Bundle/LichessBundle/Resources/public/js/ctrl.2.js
+++ src/Bundle/LichessBundle/Resources/public/js/ctrl.2.js
@@ -69,15 +69,15 @@ $(function()
             showTab: false
         });
     });
-
-    //analytics
-    if(document.domain == 'lichess.org') {
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-7935029-3']);
-        _gaq.push(['_trackPageview']);
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = 'http://www.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-    }
 });
+
+//analytics
+if(document.domain == 'lichess.org') {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-7935029-3']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = 'http://www.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+}
