ab9726b4fd9e6720e38d77c3dd626c2b3c57b858
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-24 15:58:54 +0200

    Add play with anybody invitation timeout functional tests

--- src/Bundle/LichessBundle/Tests/Controllers/GameControllerTest.php
+++ src/Bundle/LichessBundle/Tests/Controllers/GameControllerTest.php
@@ -96,6 +96,46 @@ class GameControllerTest extends WebTestCase
         $this->assertEquals('Human opponent', $crawler->filter('div.lichess_opponent span')->text());
     }
 
+    public function testPlayWithAnybodyTimeout()
+    {
+        $client = $this->createClient();
+        @unlink($client->getContainer()->getParameter('lichess.anybody.connection_file'));
+
+        // player1 creates a new game
+        $crawler = $client->request('GET', '/');
+        preg_match('#"player":\{"fullHash":"([\w\d]{10})"#', $client->getResponse()->getContent(), $match);
+        $player1Hash = $match[1];
+
+        // player1 invites anybody
+        $crawler = $client->click($crawler->filter('a:contains("Play with anybody")')->link());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(0, $crawler->filter('div.lichess_chat')->count());
+        $this->assertEquals(1, $crawler->filter('div.lichess_board')->count());
+        $this->assertEquals(1, $crawler->filter('div.lichess_table_wait_anybody')->count());
+        $this->assertEquals(1, $crawler->filter('div.lichess_player_white')->count());
+
+        // player1 disconnects
+        $game = $client->getContainer()->getLichessPersistenceService()->find(substr($player1Hash, 0, 6));
+        $player1 = $game->getPlayerByHash(substr($player1Hash, 6, 4));
+        $player1->setTime(time() - $client->getContainer()->getParameter('lichess.synchronizer.timeout') -1);
+        $client->getContainer()->getLichessPersistenceService()->save($game);
+
+        // player2 creates a new game
+        $crawler = $client->request('GET', '/');
+
+        // player2 invites anybody
+        $crawler = $client->click($crawler->filter('a:contains("Play with anybody")')->link());
+        //$this->assertRoute('lichess_anybody', $client);
+        $this->assertFalse($client->getResponse()->isRedirection());
+
+        // player2 sees the wait anybody page
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(0, $crawler->filter('div.lichess_chat')->count());
+        $this->assertEquals(1, $crawler->filter('div.lichess_board')->count());
+        $this->assertEquals(1, $crawler->filter('div.lichess_table_wait_anybody')->count());
+        $this->assertEquals(1, $crawler->filter('div.lichess_player_white')->count());
+    }
+
     public function testJoinStartedGame()
     {
         // player1 creates a new game
@@ -113,6 +153,12 @@ class GameControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/'.$gameHash);
         $this->assertEquals(410, $client->getResponse()->getStatusCode());
         $this->assertEquals(1, $crawler->filter('h1:contains("This game has 2 players")')->count());
+    }
+
+    protected function assertRoute($route, $client)
+    {
+        $profiler = $this->getProfiler($client->getResponse());
+        $this->assertEquals($route, $profiler['app']->getRoute());
     }
 }
 
