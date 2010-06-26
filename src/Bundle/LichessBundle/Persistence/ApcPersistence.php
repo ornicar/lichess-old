09e4cd99b010a4fca09011493d4c1617e40a7bec
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-26 02:24:36 +0200

    Start implementing APC persistence - aborted because APC ttl would lose the games, or we save them too with the FilePersistence

---
+++ src/Bundle/LichessBundle/Persistence/ApcPersistence.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Bundle\LichessBundle\Persistence;
+
+class ApcPersistence implements PersistenceInterface
+{
+    protected $timeout;
+
+    public function __construct($dir, $timeout)
+    {
+        parent::__construct($dir);
+        $this->timeout = $timeout;
+    }
+
+    public function save($game)
+    {
+        $data = serialize($game);
+        $data = $this->encode($data);
+        $file = $this->getGameFile($game);
+        if(!file_put_contents($file, $data))
+        {
+            throw new Exception('Can not save game '.$game->getHash().' to '.$this->getGameFile($game));
+        }
+        
+        $game->setUpdatedAt(time());
+    }
+
+    /**
+     * @param string $hash
+     * @return Game
+     */
+    public function find($hash)
+    {
+        $file = $this->dir.'/'.$hash;
+
+        if(!\file_exists($file)) {
+            throw new \Exception('Game file '.$file.' does not exist');
+        }
+
+        $data = file_get_contents($file);
+        $data = $this->decode($data);
+        $game = \unserialize($data);
+
+        if(!$game) {
+            return null;
+        }
+
+        $game->setUpdatedAt(filemtime($file));
+
+        return $game;
+    }
+
+    protected function encode($data)
+    {
+        return gzcompress($data, 1);
+    }
+
+    protected function decode($data)
+    {
+        return gzuncompress($data);
+    }
+
+    public function getUpdatedAt($hash)
+    {
+        $file = $this->dir.'/'.$hash;
+
+        if(!\file_exists($file))
+        {
+            throw new \Exception('Game file '.$file.' does not exist');
+        }
+
+        return filemtime($file);
+    }
+
+    public function getGameFile($game)
+    {
+        return $this->dir.'/'.$game->getHash();
+    }
+}
