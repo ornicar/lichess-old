91730f90ffa70755c9d6b79f5a229a1418bbf086
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-29 00:54:40 +0200

    Don't show other player pieces while game ain't started

--- src/Bundle/LichessBundle/Resources/views/Game/board.php
+++ src/Bundle/LichessBundle/Resources/views/Game/board.php
@@ -2,10 +2,12 @@
 
 $board = $player->getGame()->getBoard();
 $squares = $board->getSquares();
+$isGameStarted = $player->getGame()->getIsStarted();
+$turnPlayer = $player->getGame()->getTurnPlayer();
 
 if ($player->isBlack())
 {
-  $squares = array_reverse($squares, true);
+    $squares = array_reverse($squares, true);
 }
 
 $x = $y = 1;
@@ -14,24 +16,26 @@ print '<div class="lichess_board">';
 
 foreach($squares as $squareKey => $square)
 {
-  printf('<div class="lcs %s%s" id="%s" style="top:%dpx;left:%dpx;">',
-      $square->getColor(), $checkSquareKey === $squareKey ? ' check' : '', $squareKey, 64*(8-$x), 64*($y-1)
-  );
+    printf('<div class="lcs %s%s" id="%s" style="top:%dpx;left:%dpx;">',
+        $square->getColor(), $checkSquareKey === $squareKey ? ' check' : '', $squareKey, 64*(8-$x), 64*($y-1)
+    );
 
     print '<div class="lcsi"></div>';
 
     if($piece = $board->getPieceByKey($squareKey)) {
-      printf('<div class="lichess_piece %s %s"></div>',
-        strtolower($piece->getClass()), $piece->getColor()
-      );
+        if($isGameStarted || $piece->getPlayer() === $player) {
+            printf('<div class="lichess_piece %s %s"></div>',
+                strtolower($piece->getClass()), $piece->getColor()
+            );
+        }
     }
 
-  print '</div>';
-  
-  if (++$x === 9)
-  {
-    $x = 1; ++$y;
-  }
+    print '</div>';
+
+    if (++$x === 9)
+    {
+        $x = 1; ++$y;
+    }
 }
 
 print '</div>';
