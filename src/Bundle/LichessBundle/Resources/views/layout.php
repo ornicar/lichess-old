f94ac46fcec33f022f1b5ed6d2e0345679198629
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-29 22:55:44 +0200

    Update assets

--- src/Bundle/LichessBundle/Resources/views/layout.php
+++ src/Bundle/LichessBundle/Resources/views/layout.php
@@ -4,7 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Lichess | Open Source Chess game</title>
         <link type="text/css" rel="stylesheet" href="/bundle/lichess/css/reset.css" />
-        <link type="text/css" rel="stylesheet" href="/bundle/lichess/css/lichess.14.css" />
+        <link type="text/css" rel="stylesheet" href="/bundle/lichess/css/lichess.15.css" />
         <meta content="Free online Chess game. Easy and fast: no registration, no flash; just sit and play. Open Source software, uses PHP 5.3, Symfony2 and jQuery 1.4" name="description">
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="google-site-verification" content="fZ08Imok7kcLaGcJg7BKQExO6vXGgSgsJUsW6JalUCo" />
@@ -45,6 +45,6 @@
         </div>
         <script src="/bundle/lichess/js/lib.3.min.js" type="text/javascript"></script>
         <script src="/bundle/lichess/js/ctrl.9.js" type="text/javascript"></script>
-        <script src="/bundle/lichess/js/game.14.js" type="text/javascript"></script>
+        <script src="/bundle/lichess/js/game.15.js" type="text/javascript"></script>
     </body>
 </html>
