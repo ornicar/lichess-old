941182dcd43d7662768db10d2634f35ef540c64b
ornicar <thibault.duplessis@gmail.com>
2010-06-29 10:19:00 +0200

    Fix static board layout

--- src/Bundle/LichessBundle/Resources/views/Main/index.php
+++ src/Bundle/LichessBundle/Resources/views/Main/index.php
@@ -1,6 +1,6 @@
 <?php $view->extend('LichessBundle::layout') ?>
 
-<div class="lichess_game lichess_game_not_started clearfix lichess_player_<?php echo $color ?>">
+<div class="lichess_game_not_started clearfix lichess_player_<?php echo $color ?>">
     <div class="lichess_board_wrap">
         <?php $view->output('LichessBundle:Main:staticBoard', array('color' => $color)) ?>
         <a href="<?php echo $view->router->generate('lichess_homepage', array('color' => 'white' === $color ? 'black' : 'white')) ?>" class="lichess_exchange" title="Change position"></a>
