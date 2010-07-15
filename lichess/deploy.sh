adf6f3d6d7bbca8ce26254ccb04d019725a66d86
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-07-15 20:48:42 +0200

    Remove the deployment script

--- lichess/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Usage: ./deploy.sh username@server
-rsync --archive --force --delete --progress --compress --checksum --exclude-from=lichess/config/rsync_exclude.txt -e "ssh -i /home/thib/.ssh/lichess.pem" ./ $1
