746d03aff08481ef348bc1e4485a6c1b4c2f5857
ornicar <thibault.duplessis@gmail.com>
2010-07-20 23:17:32 +0200

    Remove unused templates

--- src/Bundle/LichessBundle/Resources/views/Game/alreadyStarted.php
+++
@@ -1,8 +0,0 @@
-<?php $view->extend('LichessBundle::layout') ?>
-
-<div class="lichess_box">
-    <h1 class="lichess_title">This game has 2 players</h1>
-    <img src="/bundle/lichess/images/sprite.png" alt="Open source Chess game" />
-    <p>You cannot join this game, because it is already started!</p>
-    <a class="lichess_new_game" href="<?php echo $view->router->generate('lichess_homepage') ?>">Start a new game</a>
-</div>
