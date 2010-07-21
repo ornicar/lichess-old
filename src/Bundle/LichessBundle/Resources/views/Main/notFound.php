0312c177485a71481061a98cadb19f7f0aaa93fe
ornicar <thibault.duplessis@gmail.com>
2010-07-22 00:53:05 +0200

    Add missing french translations

--- src/Bundle/LichessBundle/Resources/views/Main/notFound.php
+++ src/Bundle/LichessBundle/Resources/views/Main/notFound.php
@@ -1,8 +1,8 @@
 <?php $view->extend('LichessBundle::layout') ?>
 
 <div class="lichess_box">
-    <h1 class="lichess_title">Page not found (404)</h1>
-    <img src="/bundle/lichess/images/sprite.png" alt="Open source Chess game" />
-    <p>There is nothing to see here. If you think it's a bug, please send a mail to <span class="js_email"></span></p>
-    <a class="lichess_new_game" href="<?php echo $view->router->generate('lichess_homepage') ?>">Start a new game</a>
+    <h1 class="lichess_title"><?php echo $view->translator->_('Page not found') ?> (404)</h1>
+    <img src="/bundle/lichess/images/sprite.png" alt="Open source online Chess game" />
+    <p><?php echo $view->translator->_("There is nothing to see here. If you think it's a bug, please send a mail to %email%", array('%email%' => '<span class="js_email"></span>')) ?></p>
+    <a class="lichess_new_game" href="<?php echo $view->router->generate('lichess_homepage') ?>"><?php echo $view->translator->_('Play a new game') ?></a>
 </div>
