75b4bebd311c854dda19bf6d88677f432da73df6
ornicar <thibault.duplessis@gmail.com>
2010-07-24 20:51:00 +0200

    Specify current locale in HTML document

--- src/Bundle/LichessBundle/Resources/views/layout.php
+++ src/Bundle/LichessBundle/Resources/views/layout.php
@@ -11,9 +11,10 @@
     endif;
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="<?php echo $view->session->getLocale() ?>">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Language" content="<?php echo $view->session->getLocale() ?>">
         <title><?php $view->slots->output('title', 'Lichess') ?> | online free Chess game</title>
         <meta content="Free online Chess game. Easy and fast: no registration, no ads, no flash. Play Chess with computer, friends or random opponent. OpenSource software, uses PHP 5.3, Symfony2 and jQuery 1.4" name="description">
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
