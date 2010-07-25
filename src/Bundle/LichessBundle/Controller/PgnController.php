15aa8dfb1609d155dc4f73f699c6b98e15960370
ornicar <thibault.duplessis@gmail.com>
2010-07-25 18:14:01 +0200

    Add PGN analysis routing, controller and view

--- src/Bundle/LichessBundle/Controller/PgnController.php
+++ src/Bundle/LichessBundle/Controller/PgnController.php
@@ -4,11 +4,37 @@ namespace Bundle\LichessBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller;
 use Symfony\Components\HttpKernel\Exception\NotFoundHttpException;
+use Bundle\LichessBundle\Notation\PgnDumper;
 
 class PgnController extends Controller
 {
-    public function testAction()
+    public function analyseAction($hash, $color)
     {
-        return $this->render('LichessBundle:Pgn:test');
+        $game = $this->findGame($hash);
+        $dumper = new PgnDumper();
+        $pgn = $dumper->dumpGame($game);
+
+        return $this->render('LichessBundle:Pgn:analyse', array(
+            'game' => $game,
+            'color' => $color,
+            'pgn' => $pgn
+        ));
+    }
+
+    /**
+     * Return the game for this hash 
+     * 
+     * @param string $hash 
+     * @return Game
+     */
+    protected function findGame($hash)
+    {
+        $game = $this->container->getLichessPersistenceService()->find($hash);
+
+        if(!$game) {
+            throw new NotFoundHttpException('Can\'t find game '.$hash);
+        } 
+
+        return $game;
     }
 }
--- src/Bundle/LichessBundle/Resources/views/Game/goodies.php
+++ src/Bundle/LichessBundle/Resources/views/Game/goodies.php
@@ -1,4 +1,5 @@
 <div class="lichess_goodies">
     <?php echo $view->translator->_('Public permalink') ?> 
     <input class="lichess_hash_input" readonly="readonly" value="<?php echo $view->router->generate('lichess_game', array('hash' => $game->getHash()), true) ?>" title="Share this url to let spectators see the game" />
+    <a href="<?php echo $view->router->generate('lichess_pgn_viewer', array('hash' => $game->getHash(), 'color' => 'white')) ?>"><?php echo $view->translator->_('Replay and analyse') ?></a>
 </div>
