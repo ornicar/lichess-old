b0a72f4fac98c9ab66f807d1435e9acbde81db25
ornicar <thibault.duplessis@gmail.com>
2010-07-25 20:42:25 +0200

    Update all translations

--- src/Bundle/LichessBundle/Resources/views/Pgn/analyse.php
+++ src/Bundle/LichessBundle/Resources/views/Pgn/analyse.php
@@ -1,6 +1,6 @@
 <?php $view->extend('LichessBundle::layout') ?>
 <?php $view->slots->set('assets_pack', 'analyse') ?>
-<?php $view->slots->set('title', 'Lichess - '.$view->translator->_('Replay and analysis')) ?>
+<?php $view->slots->set('title', 'Lichess - '.$view->translator->_('Replay and analyse')) ?>
 
 <textarea id="pgnText"><?php echo $pgn ?></textarea>
 <div class="analyse clearfix">
@@ -9,7 +9,7 @@
         <div id="GameButtons"></div> 
     </div>
     <div class="moves_wrap">
-        <h1><?php echo $view->translator->_('Replay and analysis') ?></h1>
+        <h1><?php echo $view->translator->_('Replay and analyse') ?></h1>
         <div id="GameText"></div>
     </div>
 </div>
