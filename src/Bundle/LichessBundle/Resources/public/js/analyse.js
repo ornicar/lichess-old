4a69d8d9286dcef5c1653614546f21eedb2e4892
ornicar <thibault.duplessis@gmail.com>
2010-07-27 19:41:11 +0200

    Remove animation when changing sides in view mode

--- src/Bundle/LichessBundle/Resources/public/js/analyse.js
+++ src/Bundle/LichessBundle/Resources/public/js/analyse.js
@@ -11,16 +11,8 @@ function customFunctionOnPgnGameLoad() {
         it++;
         if(0 == it%3) $(this).addClass('break');
     });
-
-    function rotateBoard(duration) {
-        $('#GameBoard').animate({ rotate: '+=180deg' }, duration);
-        $('#GameBoard img.pieceImage').animate({ rotate: '-=180deg' }, duration);
-    }
     $('div.lichess_goodies a.rotate_board').click(function() {
-        rotateBoard(1000);
+        $('#GameBoard').toggleClass('flip');
         return false;
     });
-    if($('div.lichess_goodies a.rotate_board').hasClass('black')) {
-        rotateBoard(1);
-    }
 }
