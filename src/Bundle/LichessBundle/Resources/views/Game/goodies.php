83b85555b19fc31a6647329bb158106bd93f54e7
ornicar <thibault.duplessis@gmail.com>
2010-07-27 23:46:14 +0200

    Remove permalink input

--- src/Bundle/LichessBundle/Resources/views/Game/goodies.php
+++ src/Bundle/LichessBundle/Resources/views/Game/goodies.php
@@ -1,4 +1,3 @@
 <div class="lichess_goodies">
-    <input class="lichess_hash_input" readonly="readonly" value="<?php echo $view->router->generate('lichess_game', array('hash' => $game->getHash()), true) ?>" title="Share this url to let spectators see the game" />
-    <a class="lichess_replay_link" target="_blank" href="<?php echo $view->router->generate('lichess_pgn_viewer', array('hash' => $game->getHash(), 'color' =>isset($color) ? $color : 'white')) ?>"><?php echo $view->translator->_('Replay and analyse') ?></a>
+    <a class="lichess_replay_link" target="_blank" href="<?php echo $view->router->generate('lichess_pgn_viewer', array('hash' => $game->getHash(), 'color' =>isset($color) ? $color : 'white')) ?>" title="<?php echo $view->translator->_('Share this url to let spectators see the game') ?>"><?php echo $view->translator->_('Replay and analyse') ?></a>
 </div>
