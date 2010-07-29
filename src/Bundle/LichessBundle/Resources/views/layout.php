d52b390a0920f798ad8856d136d2d8ac5ca337b6
ornicar <thibault.duplessis@gmail.com>
2010-07-29 02:14:09 +0200

    Add support for mobile devices like android and iphone

--- src/Bundle/LichessBundle/Resources/views/layout.php
+++ src/Bundle/LichessBundle/Resources/views/layout.php
@@ -1,5 +1,5 @@
 <?php
-    $view->assets->setVersion(20);
+    $view->assets->setVersion(21);
     $view->stylesheets->add('bundle/lichess/css/lib.min.css');
     $view->stylesheets->add('bundle/lichess/css/lichess.css');
 
