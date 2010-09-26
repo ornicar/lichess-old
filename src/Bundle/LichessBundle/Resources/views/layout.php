f8e38480f66bb5707a9e1769a2f32b07c4282fcc
ornicar <thibault.duplessis@gmail.com>
2010-09-26 22:52:18 +0200

    Increment assets version number

--- src/Bundle/LichessBundle/Resources/views/layout.php
+++ src/Bundle/LichessBundle/Resources/views/layout.php
@@ -1,5 +1,5 @@
 <?php
-    $view['assets']->setVersion(21);
+    $view['assets']->setVersion(22);
     $view['stylesheets']->add('bundle/lichess/css/lib.min.css');
     $view['stylesheets']->add('bundle/lichess/css/lichess.css');
 
