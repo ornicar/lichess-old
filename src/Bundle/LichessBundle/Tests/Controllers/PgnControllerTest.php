eaac413dbfb8532389c4501e4de8c9aea22f6f75
ornicar <thibault.duplessis@gmail.com>
2010-10-04 10:59:23 +0200

    Add tests for PGN dumper, make it generate the game analysis url

---
+++ src/Bundle/LichessBundle/Tests/Controllers/PgnControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bundle\LichessBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PgnControllerTest extends WebTestCase
+{
+    public function testExportAction()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/ai/white');
+        $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $hash = preg_replace('#^.+([\w\d]{10}+)$#', '$1', $client->getRequest()->getUri());
+        $publicHash = substr($hash, 0, 6);
+        $crawler = $client->request('GET', '/analyse/'.$publicHash);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $client->click($crawler->selectLink('Export PGN')->link());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $site = 'http://localhost/analyse/'.$publicHash;
+        $expected = <<<EOF
+[Site "$site"]
+[Result "*"]
+
+*
+EOF;
+        $this->assertEquals($expected, $client->getResponse()->getContent());
+    }
+}
