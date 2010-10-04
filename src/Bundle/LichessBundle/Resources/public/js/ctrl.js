ab08ab94f4b5116ff52eb83f711642cbc2bc66f4
ornicar <thibault.duplessis@gmail.com>
2010-10-05 00:59:48 +0200

    Remove chartbeat js

--- src/Bundle/LichessBundle/Resources/public/js/ctrl.js
+++ src/Bundle/LichessBundle/Resources/public/js/ctrl.js
@@ -76,21 +76,6 @@ $.ajax = function(o) {
 }
 
 if(document.domain == 'lichess.org') {
-    // chartbeat
-    var _sf_async_config={uid:2506,domain:"lichess.org"};
-    (function(){
-    function loadChartbeat() {
-        window._sf_endpt=(new Date()).getTime();
-        var e = document.createElement('script');
-        e.setAttribute('language', 'javascript');
-        e.setAttribute('type', 'text/javascript');
-        e.setAttribute('src', "http://static.chartbeat.com/js/chartbeat.js");
-        document.body.appendChild(e);
-    }
-    var oldonload = window.onload;
-    window.onload = (typeof window.onload != 'function') ?
-        loadChartbeat : function() { oldonload(); loadChartbeat(); };
-    })();
     //analytics
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-7935029-3']);
