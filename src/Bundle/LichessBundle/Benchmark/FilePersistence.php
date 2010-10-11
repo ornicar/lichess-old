3b62670f3787e4483b85f77ccfc5564854c8c216
ornicar <thibault.duplessis@gmail.com>
2010-10-11 22:39:16 +0200

    Add FilePersistence and MongoDBPersistence benchmarks

--- src/Bundle/LichessBundle/Benchmark/FilePersistence.php
+++ src/Bundle/LichessBundle/Benchmark/FilePersistence.php
@@ -1,19 +1,24 @@
 <?php
 
 use Bundle\LichessBundle\Persistence\FilePersistence;
+use Bundle\LichessBundle\Persistence\MongoDBPersistence;
 use Bundle\LichessBundle\Chess\Generator;
 use Bundle\LichessBundle\Entities\Game;
 
 require_once(__DIR__.'/bootstrap.php');
 
-require_once(__DIR__.'/../../Persistence/PersistenceInterface.php');
-require_once(__DIR__.'/../../Persistence/FilePersistence.php');
+require_once(__DIR__.'/../Persistence/FilePersistence.php');
+require_once(__DIR__.'/../Persistence/MongoDBPersistence.php');
 
+$driver = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : 'file';
+
+$totalTime = 0;
 $dir = sys_get_temp_dir().'/lichess';
 if(!is_dir($dir)) {
     mkdir($dir);
 }
-$persistence = new FilePersistence($dir);
+
+$persistence = 'file' == $driver ? new FilePersistence($dir) : new MongoDBPersistence();
 $generator = new Generator();
 $game = $generator->createGame();
 $persistence->save($game);
@@ -32,8 +37,11 @@ $persistence->save($game);
 print "Real world game\n";
 lichess_test_performance_file_persistence($game, $persistence);
 
-function lichess_test_performance_file_persistence(Game $game, FilePersistence $persistence)
+printf('Total: %01.2f ms'."\n", $totalTime);
+
+function lichess_test_performance_file_persistence(Game $game, $persistence)
 {
+    global $totalTime;
     $hash = $game->getHash();
     $iterations = 1000;
 
@@ -41,7 +49,7 @@ function lichess_test_performance_file_persistence(Game $game, FilePersistence $
     for($it=0; $it<$iterations; $it++) {
         $persistence->find($hash);
     }
-    $time = 1000 * (microtime(true) - $start);
+    $totalTime += $time = 1000 * (microtime(true) - $start);
 
     printf('%s %d games in %01.2f ms'."\n", 'Find', $iterations, $time);
     printf('%01.2f ms per find'."\n", $time/$iterations);
@@ -50,11 +58,8 @@ function lichess_test_performance_file_persistence(Game $game, FilePersistence $
     for($it=0; $it<$iterations; $it++) {
         $persistence->save($game);
     }
-    $time = 1000 * (microtime(true) - $start);
+    $totalTime += $time = 1000 * (microtime(true) - $start);
 
     printf('%s %d games in %01.2f ms'."\n", 'Save', $iterations, $time);
     printf('%01.2f ms per save'."\n", $time/$iterations);
-
-    $file = $persistence->getGameFile($game);
-    printf('Size: %d B'."\n", filesize($file));
 }
