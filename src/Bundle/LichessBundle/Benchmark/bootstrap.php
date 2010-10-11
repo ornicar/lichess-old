fb6152057564666aca6a1fdd29622c3865889e9c
ornicar <thibault.duplessis@gmail.com>
2010-10-11 22:39:16 +0200

    Add FilePersistence and MongoDBPersistence benchmarks

--- src/Bundle/LichessBundle/Benchmark/bootstrap.php
+++ src/Bundle/LichessBundle/Benchmark/bootstrap.php
@@ -1,18 +1,19 @@
 <?php
 
-require_once(__DIR__.'/../../Entities/Game.php');
-require_once(__DIR__.'/../../Entities/Chat/Room.php');
-require_once(__DIR__.'/../../Entities/Player.php');
-require_once(__DIR__.'/../../Entities/Piece.php');
-require_once(__DIR__.'/../../Entities/Piece/Pawn.php');
-require_once(__DIR__.'/../../Entities/Piece/Rook.php');
-require_once(__DIR__.'/../../Entities/Piece/Knight.php');
-require_once(__DIR__.'/../../Entities/Piece/Bishop.php');
-require_once(__DIR__.'/../../Entities/Piece/Queen.php');
-require_once(__DIR__.'/../../Entities/Piece/King.php');
+require_once(__DIR__.'/../Entities/Game.php');
+require_once(__DIR__.'/../Entities/Chat/Room.php');
+require_once(__DIR__.'/../Entities/Player.php');
+require_once(__DIR__.'/../Entities/Piece.php');
+require_once(__DIR__.'/../Entities/Piece/Pawn.php');
+require_once(__DIR__.'/../Entities/Piece/Rook.php');
+require_once(__DIR__.'/../Entities/Piece/Knight.php');
+require_once(__DIR__.'/../Entities/Piece/Bishop.php');
+require_once(__DIR__.'/../Entities/Piece/Queen.php');
+require_once(__DIR__.'/../Entities/Piece/King.php');
 
-require_once(__DIR__.'/../../Chess/Generator.php');
-require_once(__DIR__.'/../../Chess/Board.php');
-require_once(__DIR__.'/../../Chess/Square.php');
-require_once(__DIR__.'/../../Chess/PieceFilter.php');
-require_once(__DIR__.'/../../Chess/Analyser.php');
+require_once(__DIR__.'/../Chess/Generator.php');
+require_once(__DIR__.'/../Chess/Board.php');
+require_once(__DIR__.'/../Chess/Square.php');
+require_once(__DIR__.'/../Chess/PieceFilter.php');
+require_once(__DIR__.'/../Chess/Analyser.php');
+require_once(__DIR__.'/../Stack.php');
