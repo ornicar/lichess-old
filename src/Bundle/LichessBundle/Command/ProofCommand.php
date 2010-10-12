cfbddccf05af1ddf0de4d7e8d729488305db0c04
ornicar <thibault.duplessis@gmail.com>
2010-10-12 19:09:21 +0200

    Improve ProofCommand

--- src/Bundle/LichessBundle/Command/ProofCommand.php
+++ src/Bundle/LichessBundle/Command/ProofCommand.php
@@ -34,26 +34,38 @@ class ProofCommand extends BaseCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $player = $this->findPlayer($input->getArgument('hash'));
-        $opponent = $player->getOpponent();
-        $output->writeLn(sprintf('Opponent hash is %s', $opponent->getFullHash()));
-        $message = '--- please believe me ---';
-        $player->getGame()->getRoom()->addMessage($opponent->getColor(), $message);
-        $htmlMessage = \Bundle\LichessBundle\Helper\TextHelper::autoLink(htmlentities($message, ENT_COMPAT, 'UTF-8'));
-        $sayEvent = array(
-            'type' => 'message',
-            'html' => sprintf('<li class="%s">%s</li>', $opponent->getColor(), $htmlMessage)
-        );
-        $player->getStack()->addEvent($sayEvent);
-        $opponent->getStack()->addEvent($sayEvent);
-        $this->container->getLichessPersistenceService()->save($player->getGame());
+        $hash = $input->getArgument('hash');
+        if(6 === strlen($hash)) {
+            $game = $this->container->getLichessPersistenceService()->find($hash);
+            if(!$game) {
+                throw new \Exception('Can\'t find game '.$hash);
+            }
+            foreach(array('white', 'black') as $color) {
+                $output->writeLn(sprintf('%s: %s', $color, $game->getPlayer($color)->getFullHash()));
+            }
+        }
+        else {
+            $player = $this->findPlayer($input->getArgument('hash'));
+            $opponent = $player->getOpponent();
+            $output->writeLn(sprintf('Opponent hash is %s', $opponent->getFullHash()));
+            $message = '--- please believe me ---';
+            $player->getGame()->getRoom()->addMessage($opponent->getColor(), $message);
+            $htmlMessage = \Bundle\LichessBundle\Helper\TextHelper::autoLink(htmlentities($message, ENT_COMPAT, 'UTF-8'));
+            $sayEvent = array(
+                'type' => 'message',
+                'html' => sprintf('<li class="%s">%s</li>', $opponent->getColor(), $htmlMessage)
+            );
+            $player->getStack()->addEvent($sayEvent);
+            $opponent->getStack()->addEvent($sayEvent);
+            $this->container->getLichessPersistenceService()->save($player->getGame());
+        }
         $output->writeLn('Done.');
     }
 
     /**
-     * Get the player for this hash 
-     * 
-     * @param string $hash 
+     * Get the player for this hash
+     *
+     * @param string $hash
      * @return Player
      */
     protected function findPlayer($hash)
@@ -64,12 +76,12 @@ class ProofCommand extends BaseCommand
         $game = $this->container->getLichessPersistenceService()->find($gameHash);
         if(!$game) {
             throw new \Exception('Can\'t find game '.$gameHash);
-        } 
+        }
 
         $player = $game->getPlayerByHash($playerHash);
         if(!$player) {
             throw new \Exception('Can\'t find player '.$playerHash);
-        } 
+        }
 
         return $player;
     }
