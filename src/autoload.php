9d5bc063f2c3f4d2137e87559d8d7eff38ce0e4c
ornicar <thibault.duplessis@gmail.com>
2010-10-13 03:07:33 +0200

    Autoload Zend Framework

--- src/autoload.php
+++ src/autoload.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/vendor/Symfony/src/Symfony/Component/HttpFoundation/Unive
 
 $loader = new Symfony\Component\HttpFoundation\UniversalClassLoader();
 $loader->registerNamespaces(array(
+    'Zend'                   => __DIR__.'/vendor/zend/library',
     'Symfony'                => __DIR__.'/vendor/Symfony/src',
     'Doctrine\\Common'       => __DIR__.'/vendor/mongodb-odm/lib/vendor/doctrine-common/lib',
     'Doctrine\\ODM\\MongoDB' => __DIR__.'/vendor/mongodb-odm/lib',
