e08f963a837fc6b5aa81ef1a50b1c4df1468e02c
ornicar <thibault.duplessis@gmail.com>
2010-10-17 02:40:22 +0200

    Add an anchor to the topic reply section

--- src/Application/ForumBundle/Resources/views/Post/new.php
+++ src/Application/ForumBundle/Resources/views/Post/new.php
@@ -1,5 +1,5 @@
-<h2 class="postNewTitle">Reply to this topic</h2>
-<form action="<?php echo $view['router']->generate('forum_post_create', array('topicId' => $topic->getId())) ?>" method="post">
+<h2 class="postNewTitle" id="reply">Reply to this topic</h2>
+<form action="<?php echo $view['router']->generate('forum_post_create', array('topicId' => $topic->getId())) ?>#reply" method="post">
     <label><span class="required">Message</span><textarea id="<?php echo $form['message']->getId() ?>" name="<?php echo $form['message']->getName() ?>"></textarea></label>
     <label><span>Author</span> <input class="authorName" name="<?php echo $form['authorName']->getName() ?>" /></label>
     <input type="submit" class="submit button" value="Reply" />
