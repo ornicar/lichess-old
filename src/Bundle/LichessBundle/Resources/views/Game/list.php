4867896e8081cbec1efcc1283c81e3e8b17f3ce9
ornicar <thibault.duplessis@gmail.com>
2010-10-18 01:14:20 +0200

    Remember game hashes in game list

--- src/Bundle/LichessBundle/Resources/views/Game/list.php
+++ src/Bundle/LichessBundle/Resources/views/Game/list.php
@@ -2,6 +2,6 @@
 <?php $view['slots']->set('title', 'Currently playing games') ?>
 <?php $view['slots']->set('assets_pack', 'gamelist') ?>
 
-<div class="game_list" data-url="<?php echo $view['router']->generate('lichess_games_inner') ?>">
-<?php echo $view['actions']->render('LichessBundle:Game:listInner') ?>
+<div class="game_list" data-url="<?php echo $view['router']->generate('lichess_games_inner', array('hashes' => $hashes)) ?>">
+<?php echo $view['actions']->render('LichessBundle:Game:listInner', array('hashes' => $hashes)) ?>
 </div>
