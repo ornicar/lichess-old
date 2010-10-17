feb44bd430f454cb6787c04e81bb8d2187a1dea6
ornicar <thibault.duplessis@gmail.com>
2010-10-17 02:39:37 +0200

    Remove unnecessary overwriten methods from post controller

--- src/Application/ForumBundle/Controller/PostController.php
+++ src/Application/ForumBundle/Controller/PostController.php
@@ -49,39 +49,4 @@ class PostController extends BasePostController
         return $this->redirect($url);
     }
 
-    protected function getRenderer()
-    {
-        return $this->container->getParameter('forum.template.renderer');
-    }
-
-    /**
-     * Create a PostForm instance and returns it
-     *
-     * @param string $name
-     * @param Topic $topic
-     * @return Bundle\ForumBundle\Form\PostForm
-     */
-    protected function createForm($name, Topic $topic)
-    {
-        $formClass = $this->container->getParameter('forum.post_form.class');
-        $postClass = $this['forum.post_repository']->getObjectClass();
-        $post = new $postClass();
-        $post->setTopic($topic);
-
-        return new $formClass($name, $post, $this['validator']);
-    }
-
-    /**
-     * Save a post in database
-     *
-     * @param Post $post
-     * @return null
-     **/
-    public function savePost(Post $post)
-    {
-        $objectManager = $this['forum.post_repository']->getObjectManager();
-        $objectManager->persist($post);
-        $objectManager->flush();
-    }
-
 }
