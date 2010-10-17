ba22d082ad44b6bc470a0307e3a2c2674f88eaf7
ornicar <thibault.duplessis@gmail.com>
2010-10-17 02:40:04 +0200

    Fix escaping of post author name

--- src/Application/ForumBundle/Resources/views/Post/list.php
+++ src/Application/ForumBundle/Resources/views/Post/list.php
@@ -2,7 +2,7 @@
     <?php foreach ($posts as $index => $post): ?>
     <div class="post">
         <div class="metas">
-            <span class="authorName"><?php echo $post->getAuthorName() ?: 'Anonymous' ?></span> <span class="createdAt"><?php echo $view['time']->ago($post->getCreatedAt()) ?></span>
+            <span class="authorName"><?php echo $view['lichess']->escape($post->getAuthorName()) ?: 'Anonymous' ?></span> <span class="createdAt"><?php echo $view['time']->ago($post->getCreatedAt()) ?></span>
             <a class="anchor" id="<?php echo $post->getNumber() ?>" href="<?php echo $view['forum']->urlForPost($post) ?>">#<?php echo $post->getNumber() ?></a>
         </div>
         <div class="message">
