09c7b560ba2ef8a093251c8f468e768b77c37d19
ornicar <thibault.duplessis@gmail.com>
2010-10-20 10:46:34 +0200

    Improve new game form

--- src/Bundle/LichessBundle/Form/NewGame.php
+++ src/Bundle/LichessBundle/Form/NewGame.php
@@ -4,4 +4,9 @@ namespace Bundle\LichessBundle\Form;
 
 class NewGame
 {
+    public static $timeChoices = array(
+        5, 10, 20, 30, 0
+    );
+
+    public $times = array();
 }
--- src/Bundle/LichessBundle/Form/NewGameForm.php
+++ src/Bundle/LichessBundle/Form/NewGameForm.php
@@ -8,5 +8,13 @@ class NewGameForm extends Form
 {
     public function configure()
     {
+        foreach(NewGame::$timeChoices as $timeChoice) {
+            $this->add(new CheckboxField($timeChoice, array('label' => $this->getLabel($timeChoice))));
+        }
+    }
+
+    protected function getLabel($time)
+    {
+        return $time ? '%nb% minutes/side' : 'no clock';
     }
 }
