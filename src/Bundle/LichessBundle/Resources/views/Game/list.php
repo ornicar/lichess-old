c2981d1cd312f6584b344bb2f90907e012a0a414
ornicar <thibault.duplessis@gmail.com>
2010-10-22 19:55:07 +0200

    Fix typo

--- src/Bundle/LichessBundle/Resources/views/Game/list.php
+++ src/Bundle/LichessBundle/Resources/views/Game/list.php
@@ -2,7 +2,7 @@
 <?php $view['slots']->set('title', 'Currently playing games') ?>
 <?php $view['slots']->set('assets_pack', 'gamelist') ?>
 
-<h1 class="title">Games beeing played right now</h1>
+<h1 class="title">Games being played right now</h1>
 <div class="game_list" data-url="<?php echo $view['router']->generate('lichess_games_inner', array('hashes' => $hashes)) ?>">
 <?php echo $view['actions']->render('LichessBundle:Game:listInner', array('hashes' => $hashes)) ?>
 </div>
