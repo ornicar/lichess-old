83116590df22f5db0036c47b1e08eacff96b2cb9
ornicar <thibault.duplessis@gmail.com>
2010-10-23 23:15:21 +0200

    Prevent bots from joining friend games, using JS redirection

--- src/Bundle/LichessBundle/Resources/views/Game/inviteFriend.php
+++ src/Bundle/LichessBundle/Resources/views/Game/inviteFriend.php
@@ -5,7 +5,7 @@
     <div class="lichess_board_wrap">
         <?php $view->output('LichessBundle:Main:staticBoard_'.$color.'.php') ?>
         <div class="lichess_overboard time_config">
-            <h2>Play with a friend</h2>
+            <h2><?php echo $view['translator']->_('Play with a friend') ?></h2>
             <div class="clock_config_form">
                 <form action="<?php echo $view['router']->generate('lichess_invite_friend', array('color' => $color)) ?>" method="post">
                     <div class="choices">
---
+++ src/Bundle/LichessBundle/Resources/views/Game/join.php
@@ -0,0 +1,16 @@
+<?php $view->extend('LichessBundle::layout.php') ?>
+<?php $view['slots']->set('title', 'Lichess - '.$view['translator']->_('Play with a friend').' - '.$color) ?>
+
+<div class="lichess_game_not_started waiting_opponent clearfix lichess_player_<?php echo $color ?>">
+    <div class="lichess_board_wrap">
+        <?php $view->output('LichessBundle:Main:staticBoard_'.$color.'.php') ?>
+        <div class="lichess_overboard">
+            <h2><?php echo $view['translator']->_('Play with a friend') ?></h2>
+            <p>Joining the game...</p>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+window.location.href = "<?php echo $view['router']->generate('lichess_join_game', array('hash' => $game->getHash())) ?>"
+</script>
