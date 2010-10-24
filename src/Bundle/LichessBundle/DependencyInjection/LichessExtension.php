5a303351db801bd834a8b4a22f739c4679ebdd6d
ornicar <thibault.duplessis@gmail.com>
2010-10-24 17:24:52 +0200

    Allow to replace AI class easily

--- src/Bundle/LichessBundle/DependencyInjection/LichessExtension.php
+++ src/Bundle/LichessBundle/DependencyInjection/LichessExtension.php
@@ -13,6 +13,10 @@ class LichessExtension extends Extension
         $loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
         $loader->load('config.xml');
 
+        if(isset($config['ai']['class'])) {
+            $container->setParameter('lichess.ai.class', $config['ai']['class']);
+        }
+
         if('test' === $container->getParameter('kernel.environment')) {
             $container->setAlias('session.storage', 'session.storage.test');
         }
