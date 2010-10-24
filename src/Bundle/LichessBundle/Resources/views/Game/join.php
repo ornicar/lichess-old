64cc887d604736836e1bd1456f555443dc4fc4ba
ornicar <thibault.duplessis@gmail.com>
2010-10-24 14:59:59 +0200

    Improve join game screen

--- src/Bundle/LichessBundle/Resources/views/Game/join.php
+++ src/Bundle/LichessBundle/Resources/views/Game/join.php
@@ -4,10 +4,9 @@
 <div class="lichess_game_not_started waiting_opponent clearfix lichess_player_<?php echo $color ?>">
     <div class="lichess_board_wrap">
         <?php $view->output('LichessBundle:Main:staticBoard_'.$color.'.php') ?>
-        <div class="lichess_overboard">
+        <div class="lichess_overboard wait_anybody">
             <h2><?php echo $view['translator']->_('Join the game') ?></h2>
-            <br />
-            <a class="join_redirect_url" href="<?php echo $view['router']->generate('lichess_join_game', array('hash' => $game->getHash())) ?>">Enjoy!</a>
+            <a class="join_redirect_url" href="<?php echo $view['router']->generate('lichess_join_game', array('hash' => $game->getHash())) ?>"><img src="<?php echo $view['assets']->getUrl('/bundles/lichess/images/hloader.gif') ?>" /></a>
         </div>
     </div>
 </div>
