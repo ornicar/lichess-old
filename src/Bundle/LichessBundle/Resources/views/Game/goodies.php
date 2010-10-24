3ec9fbf8e0a0d932759db881882c9e55800b66f0
ornicar <thibault.duplessis@gmail.com>
2010-10-24 18:46:17 +0200

    Open share and analyse pages with target _blank if currently playing

--- src/Bundle/LichessBundle/Resources/views/Game/goodies.php
+++ src/Bundle/LichessBundle/Resources/views/Game/goodies.php
@@ -1,7 +1,7 @@
 <div class="lichess_goodies">
-    <a class="lichess_replay_link" target="_blank" href="<?php echo $view['router']->generate('lichess_pgn_viewer', array('hash' => $game->getHash(), 'color' =>isset($color) ? $color : 'white')) ?>"><?php echo $view['translator']->_('Replay and analyse') ?></a>
+    <a class="lichess_replay_link blank_if_play" href="<?php echo $view['router']->generate('lichess_pgn_viewer', array('hash' => $game->getHash(), 'color' =>isset($color) ? $color : 'white')) ?>"><?php echo $view['translator']->_('Replay and analyse') ?></a>
     <div class="lichess_share_game">
         <?php $shareUrl = $view['router']->generate('lichess_game', array('hash' => $game->getHash()), true); ?>
-        Share: <a class="lichess_share_url" href="<?php echo $shareUrl ?>" title="<?php echo $view['translator']->_('Share this url to let spectators see the game') ?>"><?php echo $shareUrl ?></a>
+        Share: <a class="lichess_share_url blank_if_play" href="<?php echo $shareUrl ?>" title="<?php echo $view['translator']->_('Share this url to let spectators see the game') ?>"><?php echo $shareUrl ?></a>
     </div>
 </div>
--- src/Bundle/LichessBundle/Resources/views/Game/watch.php
+++ src/Bundle/LichessBundle/Resources/views/Game/watch.php
@@ -18,7 +18,7 @@
 
 <?php $view['slots']->start('goodies') ?>
 <div class="lichess_goodies">
-    <a class="lichess_replay_link" target="_blank" href="<?php echo $view['router']->generate('lichess_pgn_viewer', array('hash' => $game->getHash(), 'color' =>isset($color) ? $color : 'white')) ?>"><?php echo $view['translator']->_('Replay and analyse') ?></a>
+    <a class="lichess_replay_link" href="<?php echo $view['router']->generate('lichess_pgn_viewer', array('hash' => $game->getHash(), 'color' =>isset($color) ? $color : 'white')) ?>"><?php echo $view['translator']->_('Replay and analyse') ?></a>
     <br /><br />
     <?php echo $view['translator']->_('You are viewing this game as a spectator') ?>.<br /><br />
     <a href="<?php echo $view['router']->generate('lichess_homepage') ?>"><strong><?php echo $view['translator']->_('Play a new game') ?></strong></a>
