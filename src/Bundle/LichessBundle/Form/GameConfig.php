120c37851464322eecadfc85466d97ef587a308a
ornicar <thibault.duplessis@gmail.com>
2010-10-25 03:56:11 +0200

    Add variant choices to game config

--- src/Bundle/LichessBundle/Form/GameConfig.php
+++ src/Bundle/LichessBundle/Form/GameConfig.php
@@ -1,15 +1,27 @@
 <?php
 
 namespace Bundle\LichessBundle\Form;
+use Bundle\LichessBundle\Entities\Game;
 
 abstract class GameConfig
 {
     protected $timeChoices = array(5, 10, 20, 0);
+    protected $variantChoices = array(Game::VARIANT_STANDARD, Game::VARIANT_960);
     protected $translator;
 
     public function __construct($translator = null)
     {
         $this->translator = $translator;
+    }
+
+    public function getVariantChoices()
+    {
+        $choices = array();
+        foreach($this->variantChoices as $code) {
+            $choices[$code] = Game::getVariantName($code);
+        }
+
+        return $choices;
     }
 
     public function getTimeChoices()
