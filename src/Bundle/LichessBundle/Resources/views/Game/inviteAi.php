25f7e14ee21c4b417dfd1383a9ebafd0abe38434
ornicar <thibault.duplessis@gmail.com>
2010-10-31 01:57:44 +0200

    Style AI game config

--- src/Bundle/LichessBundle/Resources/views/Game/inviteAi.php
+++ src/Bundle/LichessBundle/Resources/views/Game/inviteAi.php
@@ -4,13 +4,13 @@
 <div class="lichess_game_not_started waiting_opponent clearfix lichess_player_<?php echo $color ?>">
     <div class="lichess_board_wrap">
         <?php $view->output('LichessBundle:Main:staticBoard_'.$color.'.php') ?>
-        <div class="lichess_overboard time_config">
+        <div class="lichess_overboard game_config">
             <h2><?php echo $view['translator']->_('Play with the machine') ?></h2>
             <div class="game_config_form">
                 <form action="<?php echo $view['router']->generate('lichess_invite_ai', array('color' => $color)) ?>" method="post">
                     <div class="variants">
                     <?php foreach($form['variant'] as $variant): ?>
-                        <div><?php echo $variant->widget() ?></div>
+                        <?php echo $variant->widget() ?>
                     <?php endforeach ?>
                     </div>
                     <button type="submit" class="submit">Start</button>
