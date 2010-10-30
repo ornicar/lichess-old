24b9b38de15ecbad4bcbf9ce31fa87da5310832b
ornicar <thibault.duplessis@gmail.com>
2010-10-31 01:56:02 +0200

    Make game configuration much more sexy thanks to jQuery UI buttons

--- src/Bundle/LichessBundle/Form/GameConfig.php
+++ src/Bundle/LichessBundle/Form/GameConfig.php
@@ -2,17 +2,10 @@
 
 namespace Bundle\LichessBundle\Form;
 use Bundle\LichessBundle\Entities\Game;
-use Bundle\LichessBundle\Chess\Clock;
 
 abstract class GameConfig
 {
     protected $timeChoices = array(2, 5, 10, 20, 0);
-    protected $translator;
-
-    public function __construct($translator = null)
-    {
-        $this->translator = $translator;
-    }
 
     abstract public function toArray();
 
@@ -30,10 +23,6 @@ abstract class GameConfig
 
     public function getTimeChoices()
     {
-        if(!$this->translator) {
-            throw new \LogicException('You must pass a translator instance');
-        }
-
         $choices = array();
         foreach($this->timeChoices as $time) {
             $choices[$time] = $this->getTimeName($time);
@@ -45,10 +34,9 @@ abstract class GameConfig
     protected function getTimeName($time)
     {
         if($time) {
-            $clock = new Clock($time * 60);
-            return $clock->getName();
+            return $time;
         }
 
-        return 'no clock - unlimited time';
+        return 'Unlimited';
     }
 }
