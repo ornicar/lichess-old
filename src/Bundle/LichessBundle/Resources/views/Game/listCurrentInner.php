c07e372265ba6f94971d8fefea3542d7c8f17a17
ornicar <thibault.duplessis@gmail.com>
2010-11-07 21:09:30 +0100

    Twigify realtime list of games beeing played

--- src/Bundle/LichessBundle/Resources/views/Game/listCurrentInner.php
+++
@@ -1,5 +0,0 @@
-<div class="game_list_inner clearfix">
-<?php foreach($games as $game): ?>
-    <div class="game_mini"><?php echo $view->render('LichessBundle:Game:mini.php', array('game' => $game)) ?></div>
-<?php endforeach ?>
-</div>
