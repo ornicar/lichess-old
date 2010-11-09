05bf50b931c016b8c0e3025c132e7522fe5bbb3d
ornicar <thibault.duplessis@gmail.com>
2010-11-09 18:10:07 +0100

    Map custom user document

--- src/Application/DoctrineUserBundle/Document/User.php
+++ src/Application/DoctrineUserBundle/Document/User.php
@@ -3,6 +3,9 @@
 namespace Application\DoctrineUserBundle\Document;
 use Bundle\DoctrineUserBundle\Document\User as BaseUser;
 
+/**
+ * @mongodb:Document(repositoryClass="Bundle\DoctrineUserBundle\Document\UserRepository")
+ */
 class User extends BaseUser
 {
 }
