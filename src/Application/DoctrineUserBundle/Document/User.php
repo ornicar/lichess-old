ea968ae9645a8587b3aca112b3a3059999800f18
ornicar <thibault.duplessis@gmail.com>
2010-11-13 23:48:36 +0100

    Implement basic security with login

--- src/Application/DoctrineUserBundle/Document/User.php
+++ src/Application/DoctrineUserBundle/Document/User.php
@@ -4,7 +4,10 @@ namespace Application\DoctrineUserBundle\Document;
 use Bundle\DoctrineUserBundle\Document\User as BaseUser;
 
 /**
- * @mongodb:Document(repositoryClass="Bundle\DoctrineUserBundle\Document\UserRepository")
+ * @mongodb:Document(
+ *   repositoryClass="Bundle\DoctrineUserBundle\Document\UserRepository",
+ *   collection="user"
+ * )
  */
 class User extends BaseUser
 {
