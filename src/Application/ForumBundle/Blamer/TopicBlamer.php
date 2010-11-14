0fcf7f085b971fd0b37b8a6653c26d0eeaffdef7
ornicar <thibault.duplessis@gmail.com>
2010-11-14 14:00:19 +0100

    Fix forum blamers

--- src/Application/ForumBundle/Blamer/TopicBlamer.php
+++ src/Application/ForumBundle/Blamer/TopicBlamer.php
@@ -3,13 +3,15 @@
 namespace Application\ForumBundle\Blamer;
 use Bundle\ForumBundle\Blamer\AbstractSecurityBlamer;
 use Bundle\ForumBundle\Blamer\BlamerInterface;
+use Symfony\Component\Security\User\AdvancedAccountInterface;
 
 class TopicBlamer extends AbstractSecurityBlamer implements BlamerInterface
 {
     public function blame($topic)
     {
-        if($user = $this->security->getUser()) {
-            $topic->setAuthor($topic);
+        $user = $this->securityContext->getUser();
+        if($user instanceof AdvancedAccountInterface && $user->hasRole('IS_AUTHENTICATED_FULLY')) {
+            $topic->setAuthor($user);
         }
     }
 }
