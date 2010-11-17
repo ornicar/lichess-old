22637e0c6436164cec9ffc25d6f06e089471e192
ornicar <thibault.duplessis@gmail.com>
2010-11-18 00:38:49 +0100

    Give users an ELO score

--- src/Application/DoctrineUserBundle/Document/User.php
+++ src/Application/DoctrineUserBundle/Document/User.php
@@ -14,6 +14,31 @@ class User extends BaseUser
     /** @validation:Regex(pattern="/^[\w\-]+$/", message="Invalid username. Please use only letters, numbers and dash", groups={"Registration","FacebookRegistration"}) */
     protected $username;
 
+    /**
+     * ELO score of the player
+     *
+     * @mongodb:Field(type="float")
+     * @var float
+     */
+    protected $elo = 1200.00;
+
+    /**
+     * @return float
+     */
+    public function getElo()
+    {
+      return $this->elo;
+    }
+
+    /**
+     * @param  float
+     * @return null
+     */
+    public function setElo($elo)
+    {
+      $this->elo = round($elo, 2);
+    }
+
     public function setUsername($username)
     {
         parent::setUsername($username);
