31b5c2c15d19c675ac6b5fe9d168378de6adc597
ornicar <thibault.duplessis@gmail.com>
2010-11-17 01:51:28 +0100

    Introduce player show action and template

--- src/Application/DoctrineUserBundle/Controller/UserController.php
+++ src/Application/DoctrineUserBundle/Controller/UserController.php
@@ -5,9 +5,18 @@ use Bundle\DoctrineUserBundle\Controller\UserController as BaseUserController;
 
 class UserController extends BaseUserController
 {
-    /**
-     * Tell the user his account is now confirmed
-     */
+    public function showAction($username)
+    {
+        $user = $this->findUserByUsername($username);
+        $critic = $this->get('lichess.critic.user');
+        $critic->setUser($user);
+
+        return $this->render('DoctrineUserBundle:User:show.'.$this->getRenderer(), array(
+            'user'   => $user,
+            'critic' => $critic
+        ));
+    }
+
     public function confirmedAction()
     {
         return $this->redirect($this->generateUrl('lichess_homepage'));
