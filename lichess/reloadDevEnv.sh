317a80303861e506914df7fcd16b317750dfa518
ornicar <thibault.duplessis@gmail.com>
2010-11-20 03:14:22 +0100

    Remove reload files, update refresh script

--- lichess/reloadDevEnv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Usage: ./lichess/reloadDevEnv.sh
-php lichess/console doctrine:database:drop &&
-php lichess/console doctrine:database:create &&
-php lichess/console doctrine:schema:create &&
-php lichess/console doctrine:generate:proxies &&
-php lichess/console doctrine:data:load &&
-echo "You're good to go!"
