4ef7fdca6f8917cd5a96267cc23190212bb1a3ff
ornicar <thibault.duplessis@gmail.com>
2010-11-20 02:01:30 +0100

    Add compatibility controller

---
+++ src/Bundle/LichessBundle/Controller/CompatController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bundle\LichessBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CompatController extends Controller
+{
+    public function gameShowAction($id)
+    {
+        $newId = $id . '00';
+
+        if($this->get('lichess.repository.game')->existsById($newId)) {
+            return $this->redirect($this->generateUrl('lichess_game', array('id' => $newId)));
+        }
+
+        throw new NotFoundHttpException(sprintf('Compatibility for game "%s" not found', $id));
+    }
+
+    public function playerShowAction($id)
+    {
+        $gameId = substr($id, 0, 6);
+        $playerId = substr($id, 6, 4);
+        $newGameId = $gameId . '00';
+
+        if($game = $this->get('lichess.repository.game')->find($newGameId)) {
+            if($player = $game->getPlayerById($playerId)) {
+                return $this->redirect($this->generateUrl('lichess_player', array('id' => $player->getFullId())));
+            }
+        }
+
+        throw new NotFoundHttpException(sprintf('Compatibility for player "%s" not found', $id));
+    }
+}
