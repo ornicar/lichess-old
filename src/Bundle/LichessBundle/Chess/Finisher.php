82ee8a9468223561c37aa04145c02cc4a033b81f
ornicar <thibault.duplessis@gmail.com>
2010-11-23 03:26:09 +0100

    Do not rate game with less than two moves

--- src/Bundle/LichessBundle/Chess/Finisher.php
+++ src/Bundle/LichessBundle/Chess/Finisher.php
@@ -17,17 +17,19 @@ class Finisher
 
     public function finish(Game $game)
     {
-        $white = $game->getPlayer('white')->getUser();
-        $black = $game->getPlayer('black')->getUser();
-        if($white && $black) {
-            if($winner = $game->getWinner()) {
-                $win = $winner->isWhite() ? -1 : 1;
-            } else {
-                $win = 0;
+        if($game->getTurns() >= 2) {
+            $white = $game->getPlayer('white')->getUser();
+            $black = $game->getPlayer('black')->getUser();
+            if($white && $black) {
+                if($winner = $game->getWinner()) {
+                    $win = $winner->isWhite() ? -1 : 1;
+                } else {
+                    $win = 0;
+                }
+                list($whiteElo, $blackElo) = $this->calculator->calculate($white->getElo(), $black->getElo(), $win);
+                $white->setElo($whiteElo);
+                $black->setElo($blackElo);
             }
-            list($whiteElo, $blackElo) = $this->calculator->calculate($white->getElo(), $black->getElo(), $win);
-            $white->setElo($whiteElo);
-            $black->setElo($blackElo);
         }
     }
 }
