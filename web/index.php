f0fdbe7fd1b529a3288a3e4f5b02aa44d6c4dfb1
ornicar <thibault.duplessis@gmail.com>
2010-11-28 18:41:32 +0100

    Remove index.php redirection

--- web/index.php
+++ web/index.php
@@ -1,13 +1,5 @@
 <?php
 
-// Redirect /index.php to /
-if(0 === strncmp($_SERVER['REQUEST_URI'], '/index.php', 10)) {
-    $url = substr($_SERVER['REQUEST_URI'], 10);
-    header('Location: '.$url, true, 301);
-    printf('<html><head><meta http-equiv="refresh" content="1;url=%s"/></head></html>', htmlspecialchars($url, ENT_QUOTES));
-    die;
-}
-
 // Try PreKernelCache
 require_once __DIR__.'/../src/Bundle/LichessBundle/PreKernelCache.php';
 
