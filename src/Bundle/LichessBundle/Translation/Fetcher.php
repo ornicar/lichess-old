19da2fa9d5acd5d70eedc9dbdfbc0b4c21ad595e
ornicar <thibault.duplessis@gmail.com>
2010-11-29 11:30:37 +0100

    Add basic fetcher implementation with configurable remote domain

---
+++ src/Bundle/LichessBundle/Translation/Fetcher.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bundle\LichessBundle\Translation;
+
+class Fetcher
+{
+    protected $domain;
+    protected $path = '/translate/list.json';
+    protected $protocol = 'http://';
+
+    public function __construct($domain)
+    {
+        $this->domain = trim($domain, '/');
+    }
+
+    public function fetch()
+    {
+        $url = $this->getUrl();
+        $json = file_get_contents($url);
+        $translations = json_decode($json, true);
+
+        return count($translations);
+    }
+
+    public function getUrl()
+    {
+        return $this->protocol.$this->domain.$this->path;
+    }
+}
