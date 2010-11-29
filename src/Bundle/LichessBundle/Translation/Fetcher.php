c6fd6897bfc65fca1d49276d4186f69c87ce310b
ornicar <thibault.duplessis@gmail.com>
2010-11-29 12:05:06 +0100

    Create one branch per translation

--- src/Bundle/LichessBundle/Translation/Fetcher.php
+++ src/Bundle/LichessBundle/Translation/Fetcher.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Bundle\LichessBundle\Translation;
+use phpGitRepo;
+
+require_once __DIR__.'/../../../vendor/php-git-repo/lib/phpGitRepo.php';
 
 class Fetcher
 {
@@ -18,6 +21,20 @@ class Fetcher
         $url = $this->getUrl();
         $json = file_get_contents($url);
         $translations = json_decode($json, true);
+
+        $repo = new phpGitRepo(__DIR__.'/../../../..');
+        $currentBranch = $repo->getCurrentBranch();
+        $repo->git('stash');
+        $repo->git('checkout master');
+        foreach($translations as $id => $translation) {
+            $branchName = 't/'.$id;
+            if(!$repo->hasBranch($branchName)) {
+                $repo->git('checkout -b '.$branchName);
+                $repo->git('checkout master');
+            }
+        }
+        $repo->git('checkout '.$currentBranch);
+        $repo->git('stash pop');
 
         return count($translations);
     }
