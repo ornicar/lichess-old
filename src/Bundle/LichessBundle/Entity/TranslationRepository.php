9b3fce9dd52cabb58ef55b70476330cee6b1be72
Juozas Kaziukenas <juozas@juokaz.com>
2010-12-05 15:44:47 +0000

    Fixed TranslationRepository

--- src/Bundle/LichessBundle/Entity/TranslationRepository.php
+++ src/Bundle/LichessBundle/Entity/TranslationRepository.php
@@ -6,8 +6,8 @@ class TranslationRepository extends ObjectRepository
 {
     public function findAllSortByCreatedAt()
     {
-        return $this->createQueryBuilder()
-            ->orderBy('createdAt', 'DESC')
+        return $this->createQueryBuilder('t')
+            ->orderBy('t.createdAt', 'DESC')
             ->getQuery()->execute();
     }
 }
