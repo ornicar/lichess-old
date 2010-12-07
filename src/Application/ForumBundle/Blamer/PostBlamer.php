98a1f190ac44e66d72513bf3c9c7a6761b0a1739
ornicar <thibault.duplessis@gmail.com>
2010-12-08 00:19:29 +0100

    Simplify forum post blamer and remove topic blamer

--- src/Application/ForumBundle/Blamer/PostBlamer.php
+++ src/Application/ForumBundle/Blamer/PostBlamer.php
@@ -3,14 +3,14 @@
 namespace Application\ForumBundle\Blamer;
 use Bundle\ForumBundle\Blamer\AbstractSecurityBlamer;
 use Bundle\ForumBundle\Blamer\BlamerInterface;
-use Symfony\Component\Security\User\AdvancedAccountInterface;
+use Bundle\DoctrineUserBundle\Model\User;
 
 class PostBlamer extends AbstractSecurityBlamer implements BlamerInterface
 {
     public function blame($post)
     {
         $user = $this->security->getUser();
-        if($user instanceof AdvancedAccountInterface && $user->hasRole('IS_AUTHENTICATED_FULLY')) {
+        if($user instanceof User) {
             $post->setAuthor($user);
         }
     }
