084e10e7097140d74a67efe9822ca50d3e3f6309
ornicar <thibault.duplessis@gmail.com>
2010-12-08 11:49:44 +0100

    Add functional tests for signin and signup

---
+++ src/Application/DoctrineUserBundle/Tests/Functional/SignupTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Application\DoctrineUserBundle\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SignupTest extends WebTestCase
+{
+    public function testSignup()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'lichess_homepage'));
+        $crawler = $client->click($crawler->filter('a:contains("Sign up")')->link());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertRegexp('/Sign up/', $crawler->filter('.lichess_title')->text());
+        $form = $crawler->selectButton('Sign up')->form();
+        $form['doctrine_user_user_form[username]'] = 'test-username';
+        $form['doctrine_user_user_form[plainPassword]'] = 'test-password';
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_show', array('username' => 'test-username')));
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertRegexp('/test-username/', $crawler->filter('.lichess_title')->text());
+    }
+
+    public function testSignupWithBadUsername()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_new'));
+        $form = $crawler->selectButton('Sign up')->form();
+        $form['doctrine_user_user_form[username]'] = 'x';
+        $form['doctrine_user_user_form[plainPassword]'] = 'test-password';
+        $client->submit($form);
+        $this->assertFalse($client->getResponse()->isRedirect());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertRegexp('/username is too short/', $client->getResponse()->getContent());
+    }
+
+    public function testSignupWithBadPassword()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_new'));
+        $form = $crawler->selectButton('Sign up')->form();
+        $form['doctrine_user_user_form[username]'] = 'test-username';
+        $form['doctrine_user_user_form[plainPassword]'] = '';
+        $client->submit($form);
+        $this->assertFalse($client->getResponse()->isRedirect());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertRegexp('/enter a password/', $client->getResponse()->getContent());
+    }
+
+    public function testSignupWithExistingUsername()
+    {
+        $client = $this->createClient();
+        $user = $client->getContainer()->get('doctrine_user.repository.user')->createUserInstance();
+        $user->setUsername('test-username');
+        $user->setPlainPassword('test-password');
+        $client->getContainer()->get('doctrine_user.object_manager')->persist($user);
+        $client->getContainer()->get('doctrine_user.object_manager')->flush();
+
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_new'));
+        $form = $crawler->selectButton('Sign up')->form();
+        $form['doctrine_user_user_form[username]'] = 'test-username';
+        $form['doctrine_user_user_form[plainPassword]'] = 'other-password';
+        $client->submit($form);
+        $this->assertFalse($client->getResponse()->isRedirect());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertRegexp('/username was already taken/', $client->getResponse()->getContent());
+    }
+
+    public function generateUrl($client, $route, $parameters = array())
+    {
+        return $client->getContainer()->get('router')->generate($route, $parameters);
+    }
+
+    public function setUp()
+    {
+        $client = $this->createClient();
+        $user = $client->getContainer()->get('doctrine_user.repository.user')->findOneByUsername('test-username');
+        if($user) {
+            $client->getContainer()->get('doctrine_user.object_manager')->remove($user);
+            $client->getContainer()->get('doctrine_user.object_manager')->flush();
+        }
+    }
+}
