55dbcd9867688671234848752e3396e986c221f8
ornicar <thibault.duplessis@gmail.com>
2010-12-27 17:18:35 +0100

    Override messenger service to update unread messages cache

---
+++ src/Application/Ornicar/MessageBundle/Messenger.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Ornicar\MessageBundle;
+
+use Bundle\Ornicar\MessageBundle\Model\Message;
+use Bundle\Ornicar\MessageBundle\Messenger as BaseMessenger;
+use Bundle\FOS\UserBundle\Model\User;
+
+class Messenger extends BaseMessenger
+{
+    public function send(Message $message)
+    {
+        parent::send($message);
+
+        $this->updateUnreadCache($message->getTo(), +1);
+    }
+
+    public function markAsRead(Message $message)
+    {
+        parent::markAsRead($message);
+
+        $this->updateUnreadCache($message->getTo(), -1);
+    }
+
+    protected function updateUnreadCache(User $user, $modifier = 0)
+    {
+        apc_store('nbm.'.$user->getUsername(), $this->messageRepository->countUnreadByUser($user) + $modifier);
+    }
+
+    public function getUnreadCacheForUsername($username)
+    {
+        return apc_fetch('nbm.'.$username);
+    }
+}
