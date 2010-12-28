cf8bf7304d7df172009b9d31b959f592a8ee22df
ornicar <thibault.duplessis@gmail.com>
2010-12-28 02:51:48 +0100

    GUI refactoring

--- src/Bundle/LichessBundle/Resources/public/js/gamelist.js
+++ src/Bundle/LichessBundle/Resources/public/js/gamelist.js
@@ -1,16 +1,16 @@
 $(function()
 {
-    var $gamelist = $('div.game_list');
-    var refreshUrl = $gamelist.attr('data-url');
-
-    // Update games
-    function reloadGameList() {
-        setTimeout(function() {
-            $.get(refreshUrl, function(html) {
-                $gamelist.html(html);
-                reloadGameList();
-            });
-        }, 2500);
-    };
-    reloadGameList();
+    if($gamelist = $('div.game_list').orNot()) {
+        refreshUrl = $gamelist.attr('data-url');
+        // Update games
+        function reloadGameList() {
+            setTimeout(function() {
+                $.get(refreshUrl, function(html) {
+                    $gamelist.html(html);
+                    reloadGameList();
+                });
+            }, 2500);
+        };
+        reloadGameList();
+    }
 });
