b0bcf18eda78f75e962a3a21667e09c2e2767026
ornicar <thibault.duplessis@gmail.com>
2011-01-10 21:21:04 +0100

    Remove TextHelper

--- src/Bundle/LichessBundle/Twig/LichessExtension.php
+++ src/Bundle/LichessBundle/Twig/LichessExtension.php
@@ -44,7 +44,6 @@ class LichessExtension extends Twig_Extension
             'lichess_session'              => 'getSession',
             'lichess_nb_connected_players' => 'getNbConnectedPlayers',
             'lichess_load_average'         => 'getLoadAverage',
-            'lichess_auto_link'            => 'autoLink',
             'lichess_user_text'            => 'user_text',
             'lichess_shorten'              => 'shorten',
             'lichess_current_url'          => 'getCurrentUrl',
@@ -294,9 +293,41 @@ class LichessExtension extends Twig_Extension
         return round($this->container->get('lichess.hardware')->getLoadAverage()).'%';
     }
 
-    public function autoLink($text)
+    protected function autoLink($text)
     {
-        return TextHelper::autoLink($text);
+        return preg_replace_callback('~
+            (                       # leading text
+                <\w+.*?>|             #   leading HTML tag, or
+                [^=!:\'"/]|           #   leading punctuation, or
+                ^                     #   beginning of line
+            )
+            (
+                (?:https?://)|        # protocol spec, or
+                (?:www\.)             # www.*
+            )
+            (
+                [-\w]+                   # subdomain or domain
+                (?:\.[-\w]+)*            # remaining subdomains or domain
+                (?::\d+)?                # port
+                (?:/(?:(?:[\~\w\+%-\@]|(?:[,.;:][^\s$]))+)?)* # path
+                (?:\?[\w\+%&=.;-]+)?     # query string
+                (?:\#[\w\-]*)?           # trailing anchor
+            )
+            ([[:punct:]]|\s|<|$)    # trailing text
+            ~x',
+            function($matches)
+            {
+                if (preg_match("/<a\s/i", $matches[1]))
+                {
+                    return $matches[0];
+                }
+                else
+                {
+                    return $matches[1].'<a href="'.($matches[2] == 'www.' ? 'http://www.' : $matches[2]).$matches[3].'" target="_blank">'.$matches[2].$matches[3].'</a>'.$matches[4];
+                }
+            },
+            $text
+        );
     }
 
     public function userText($text)
