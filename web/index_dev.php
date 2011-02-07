26a44497de7a036dc53a8da2674b66ecb8ef3779
ornicar <thibault.duplessis@gmail.com>
2011-02-06 17:18:13 -0800

    Update front controllers

--- web/index_dev.php
+++ web/index_dev.php
@@ -8,12 +8,14 @@ $start = microtime(true);
 ob_start();
 
 // Symfony2 boot
+require_once __DIR__.'/../lichess/autoload.php';
 require_once __DIR__.'/../lichess/LichessKernel.php';
+
 use Symfony\Component\HttpFoundation\Request;
 
 // Run application
 $kernel = new LichessKernel('dev', true);
-$kernel->handle(new Request())->send();
+$kernel->handle(Request::createFromGlobals())->send();
 
 // Display timer
 print str_replace('[[time]]', round(1000*(microtime(true) - $start)).'ms', ob_get_clean());
