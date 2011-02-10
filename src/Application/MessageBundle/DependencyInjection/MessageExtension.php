5f689d075ede41daa9f392467d4087fea35f652e
ornicar <thibault.duplessis@gmail.com>
2011-02-09 23:58:07 -0800

    Update DIC extension to use the new FileLocator class

--- src/Application/MessageBundle/DependencyInjection/MessageExtension.php
+++ src/Application/MessageBundle/DependencyInjection/MessageExtension.php
@@ -4,14 +4,14 @@ namespace Application\MessageBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\FileLocator;
 
 class MessageExtension extends Extension
 {
-    public function configLoad(array $config, ContainerBuilder $container)
+    public function configLoad(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('messenger.xml');
     }
 
--- src/Application/UserBundle/DependencyInjection/FOSUserExtension.php
+++ src/Application/UserBundle/DependencyInjection/FOSUserExtension.php
@@ -6,12 +6,13 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\FileLocator;
 
 class FOSUserExtension extends Extension
 {
     public function configLoad(array $config, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('online.xml');
         $loader->load('repository.xml');
         $loader->load('security.xml');
