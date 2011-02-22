3c0b09d149bae45ae63f56ecf995819ff296fdc9
ornicar <thibault.duplessis@gmail.com>
2011-02-21 21:45:37 -0800

    Use persistent clients for acceptance tests

--- src/Application/ForumBundle/Tests/Acceptance/AbstractAcceptanceTest.php
+++ src/Application/ForumBundle/Tests/Acceptance/AbstractAcceptanceTest.php
@@ -7,8 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Client;
 
 abstract class AbstractAcceptanceTest extends WebTestCase
 {
+    protected function createPersistentClient()
+    {
+        $client = $this->createClient();
+        $client->getCookieJar()->set(new Cookie(session_name(), 'test'));
+
+        return $client;
+    }
+
     protected function generateUrl(Client $client, $route, $parameters = array())
     {
         return $client->getContainer()->get('router')->generate($route, $parameters);
+    }
+
+    protected function authenticate(Client $client, $username = 'user1', $password = 'password1')
+    {
+        $client->request('POST', '/login-check', array(
+            '_username' => $username,
+            '_password' => $password
+        ));
     }
 }
--- src/Application/UserBundle/Tests/Acceptance/AbstractAcceptanceTest.php
+++ src/Application/UserBundle/Tests/Acceptance/AbstractAcceptanceTest.php
@@ -4,11 +4,28 @@ namespace Application\UserBundle\Tests\Acceptance;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Component\BrowserKit\Cookie;
 
 abstract class AbstractAcceptanceTest extends WebTestCase
 {
+    protected function createPersistentClient()
+    {
+        $client = $this->createClient();
+        $client->getCookieJar()->set(new Cookie(session_name(), 'test'));
+
+        return $client;
+    }
+
     protected function generateUrl(Client $client, $route, $parameters = array())
     {
         return $client->getContainer()->get('router')->generate($route, $parameters);
+    }
+
+    protected function authenticate(Client $client, $username = 'user1', $password = 'password1')
+    {
+        $client->request('POST', '/login-check', array(
+            '_username' => $username,
+            '_password' => $password
+        ));
     }
 }
