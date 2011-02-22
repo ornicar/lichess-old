f72809333786f4db29d56df06b278b980d710f3c
ornicar <thibault.duplessis@gmail.com>
2011-02-21 23:05:37 -0800

    Simplify DIC extension

--- src/Bundle/LichessBundle/DependencyInjection/LichessExtension.php
+++ src/Bundle/LichessBundle/DependencyInjection/LichessExtension.php
@@ -2,7 +2,7 @@
 
 namespace Bundle\LichessBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -45,26 +45,6 @@ class LichessExtension extends Extension
         if (!empty($config['test'])) {
             $loader->load('test.xml');
         }
-    }
-
-    /**
-     * Returns the base path for the XSD files.
-     *
-     * @return string The XSD base path
-     */
-    public function getXsdValidationBasePath()
-    {
-        return null;
-    }
-
-    public function getNamespace()
-    {
-        return 'http://www.symfony-project.org/schema/dic/symfony';
-    }
-
-    public function getAlias()
-    {
-        return 'lichess';
     }
 
 }
