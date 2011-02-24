f66e46b222df02d85e815e826f56ce4f36f274c9
ornicar <thibault.duplessis@gmail.com>
2011-02-24 02:09:54 -0800

    Override messenger to use the cache instead of hitting db

--- src/Application/MessageBundle/Messenger.php
+++ src/Application/MessageBundle/Messenger.php
@@ -22,13 +22,18 @@ class Messenger extends BaseMessenger
         $this->updateUnreadCache($message->getTo(), -1);
     }
 
-    protected function updateUnreadCache(User $user, $modifier = 0)
+    public function updateUnreadCache(User $user, $modifier = 0)
     {
         apc_store('nbm.'.$user->getUsername(), $this->messageRepository->countUnreadByUser($user) + $modifier);
     }
 
     public function getUnreadCacheForUsername($username)
     {
-        return apc_fetch('nbm.'.$username);
+        return apc_fetch('nbm.'.$username) ?: 0;
+    }
+
+    public function countUnreadByUser(User $user)
+    {
+        return $this->getUnreadCacheForUsername($user->getUsername());
     }
 }
