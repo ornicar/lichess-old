f5cbf6254a0f0a3a9cd285e1ed46850c89829919
ornicar <thibault.duplessis@gmail.com>
2011-02-23 22:00:08 -0800

    Fix possible issue with player blamer if no security token is available

--- src/Bundle/LichessBundle/Blamer/PlayerBlamer.php
+++ src/Bundle/LichessBundle/Blamer/PlayerBlamer.php
@@ -16,10 +16,11 @@ class PlayerBlamer
 
     public function blame(Player $player)
     {
-        $user = $this->securityContext->getToken()->getUser();
-        if($user instanceof User) {
-            $player->setUser($user);
+        if ($token = $this->securityContext->getToken()) {
+            $user = $token->getUser();
+            if($user instanceof User) {
+                $player->setUser($user);
+            }
         }
     }
 }
-
