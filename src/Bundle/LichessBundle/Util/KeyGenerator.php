403f98824b820910e67b3ac63e3bbd27774485d0
ornicar <thibault.duplessis@gmail.com>
2011-02-24 00:10:36 -0800

    Add faster random string generation with unit tests

--- src/Bundle/LichessBundle/Util/KeyGenerator.php
+++ src/Bundle/LichessBundle/Util/KeyGenerator.php
@@ -6,14 +6,16 @@ class KeyGenerator
 {
     static public function generate($length)
     {
-        $chars = 'abcdefghijklmnopqrstuvwxyz0123456789_-';
-        $nbChars = strlen($chars);
-
         $key = '';
-        for ( $i = 0; $i < $length; $i++ ) {
-            $key .= $chars[mt_rand(0, $nbChars-1)];
-        }
+        do {
+            $key .= self::fastGenerateTwelveCharsKey();
+        } while ($length > strlen($key));
 
-        return $key;
+        return substr($key, 0, $length);
+    }
+
+    static protected function fastGenerateTwelveCharsKey()
+    {
+        return base_convert(mt_rand(0x1D39D3E06400000, 0x41C21CB8E0FFFFFF), 10, 36);
     }
 }
