326714112f78f719a3b0cc7b57e640b87972ab13
ornicar <thibault.duplessis@gmail.com>
2011-02-24 02:10:12 -0800

    Register application message bundle configuration

---
+++ src/Application/MessageBundle/DependencyInjection/LichessMessageExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Application\MessageBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+class LichessMessageExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('messenger.xml');
+    }
+}
