428f1d9f9713b01da2fb011056f4b7955768f85d
ornicar <thibault.duplessis@gmail.com>
2011-02-24 17:50:58 -0800

    Add post spam detection functional test

--- src/Application/ForumBundle/Tests/Acceptance/AnonymousCreatePostAcceptanceTest.php
+++ src/Application/ForumBundle/Tests/Acceptance/AnonymousCreatePostAcceptanceTest.php
@@ -10,4 +10,16 @@ class AnonymousCreatePostAcceptanceTest extends AbstractCreatePostAcceptanceTest
         $crawler = $this->requestPostCreationPage($client);
         $this->assertEquals(1, $crawler->filter('#lichess_forum input.authorName')->count());
     }
+
+    public function testSubmitSpam()
+    {
+        $client = $this->createClient();
+        $crawler = $this->requestPostCreationPage($client);
+        $form = $crawler->selectButton('Reply')->form();
+        $form['forum_post_form[authorName]'] = 'viagra-test-123';
+        $form['forum_post_form[message]'] = 'new message';
+        $client->submit($form);
+        $this->assertFalse($client->getResponse()->isRedirect());
+        $this->assertEquals(1, $crawler->filter('#lichess_forum form')->count());
+    }
 }
