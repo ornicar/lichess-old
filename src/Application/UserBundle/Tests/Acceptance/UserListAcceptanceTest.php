7c124650bf9b6c8df0e553167f0f76a6396348bd
ornicar <thibault.duplessis@gmail.com>
2011-03-04 23:57:57 -0800

    Add functional test for user list

---
+++ src/Application/UserBundle/Tests/Acceptance/UserListAcceptanceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Application\UserBundle\Tests\Acceptance;
+
+class UserListAcceptanceTest extends AbstractAcceptanceTest
+{
+    public function testUserList()
+    {
+        list($client, $crawler) = $this->requestUserList();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertRegexp('/^Chess players/', $crawler->filter('.lichess_title')->first()->text());
+        $this->assertEquals('Who is online', $crawler->filter('.lichess_title')->last()->text());
+        $this->assertEquals('1', $crawler->filter('.pager span.current')->text());
+    }
+
+    protected function requestUserList()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'fos_user_user_list'));
+
+        return array($client, $crawler);
+    }
+}
