ef55229b9b403859ca49ca95200fc93a03ab1f34
ornicar <thibault.duplessis@gmail.com>
2011-03-04 23:57:39 -0800

    Fix synchronizer service name in user controller

--- src/Application/UserBundle/Controller/UserController.php
+++ src/Application/UserBundle/Controller/UserController.php
@@ -24,7 +24,7 @@ class UserController extends BaseUserController
     public function onlineAction($username)
     {
         $data = array();
-        $data['nbp'] = $this->container->get('lichess_synchronizer')->getNbConnectedPlayers();
+        $data['nbp'] = $this->container->get('lichess.synchronizer')->getNbConnectedPlayers();
         $data['nbm'] = $this->container->get('ornicar_message.messenger')->getUnreadCacheForUsername($username);
         $this->container->get('lichess_user.online.cache')->setUsernameOnline($username);
         $response = new Response(json_encode($data), 200, array('Content-Type' => 'application/json'));
@@ -41,7 +41,7 @@ class UserController extends BaseUserController
     public function listOnlineAction()
     {
         $users = $this->container->get('fos_user.repository.user')->findOnlineUsersSortByElo();
-        $nbPlayers = $this->container->get('lichess_synchronizer')->getNbConnectedPlayers();
+        $nbPlayers = $this->container->get('lichess.synchronizer')->getNbConnectedPlayers();
 
         return $this->container->get('templating')->renderResponse('FOSUserBundle:User:listOnline.html.twig', compact('users', 'nbPlayers'));
     }
