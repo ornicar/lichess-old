f967933269727542b32b96fd557ed0ac81df8205
ornicar <thibault.duplessis@gmail.com>
2011-03-06 13:20:46 -0800

    Remove debugging assertion in ajax queue

--- src/Bundle/LichessBundle/Resources/public/js/ajaxqueue.js
+++ src/Bundle/LichessBundle/Resources/public/js/ajaxqueue.js
@@ -9,13 +9,10 @@
     $.xqueue.prototype.add = function(url, options) {
         var self = this;
         options.xqCount = self.count++;
-        self.debug('+ '+options.xqCount);
         $.queue(document, self.name, function() {
-            self.debug('= '+options.xqCount);
             self.processing = true;
             $.isFunction(url) && (url = url());
             $.ajax(url, options).complete(function() {
-                self.debug('- '+options.xqCount);
                 self.processing = false;
                 $.dequeue(document, self.name);
             });
@@ -23,10 +20,6 @@
         if(!self.processing){
             $.dequeue(document, self.name);
         }
-    };
-
-    $.xqueue.prototype.debug = function(message) {
-        //console.debug(message);
     };
 
 })(jQuery);
