0c088987e0e67472da170d2224c106d15d49735a
ornicar <thibault.duplessis@gmail.com>
2011-03-07 19:06:10 -0800

    Fix indentation in PieceFilter

--- src/Bundle/LichessBundle/Chess/PieceFilter.php
+++ src/Bundle/LichessBundle/Chess/PieceFilter.php
@@ -7,89 +7,89 @@ use Bundle\LichessBundle\Document\Piece;
 class PieceFilter
 {
 
-  // remove dead pieces
-  public static function filterAlive(array $pieces)
-  {
-    foreach($pieces as $it => $piece) {
-      if ($piece->getIsDead()) {
-        unset($pieces[$it]);
-      }
+    // remove dead pieces
+    public static function filterAlive(array $pieces)
+    {
+        foreach($pieces as $it => $piece) {
+            if ($piece->getIsDead()) {
+                unset($pieces[$it]);
+            }
+        }
+
+        return array_values($pieces);
     }
 
-    return array_values($pieces);
-  }
+    // remove alive pieces
+    public static function filterDead(array $pieces)
+    {
+        foreach($pieces as $it => $piece) {
+            if (!$piece->getIsDead()) {
+                unset($pieces[$it]);
+            }
+        }
 
-  // remove alive pieces
-  public static function filterDead(array $pieces)
-  {
-    foreach($pieces as $it => $piece) {
-      if (!$piece->getIsDead()) {
-        unset($pieces[$it]);
-      }
+        return array_values($pieces);
     }
 
-    return array_values($pieces);
-  }
+    // only return bishop, rook and queen
+    public static function filterProjection(array $pieces)
+    {
+        foreach($pieces as $it => $piece) {
+            if (!($piece instanceof Piece\Bishop || $piece instanceof Piece\Rook || $piece instanceof Piece\Queen)) {
+                unset($pieces[$it]);
+            }
+        }
 
-  // only return bishop, rook and queen
-  public static function filterProjection(array $pieces)
-  {
-    foreach($pieces as $it => $piece) {
-      if (!($piece instanceof Piece\Bishop || $piece instanceof Piece\Rook || $piece instanceof Piece\Queen)) {
-        unset($pieces[$it]);
-      }
+        return array_values($pieces);
     }
 
-    return array_values($pieces);
-  }
+    // only keep asked class
+    public static function filterClass(array $pieces, $class)
+    {
+        foreach($pieces as $it => $piece) {
+            if (!$piece->isClass($class)) {
+                unset($pieces[$it]);
+            }
+        }
 
-  // only keep asked class
-  public static function filterClass(array $pieces, $class)
-  {
-    foreach($pieces as $it => $piece) {
-      if (!$piece->isClass($class)) {
-        unset($pieces[$it]);
-      }
+        return array_values($pieces);
     }
 
-    return array_values($pieces);
-  }
-
-  public static function filterType($pieces, $type)
-  {
-      return $this->filterClass($pieces, ucfirst($type));
-  }
-
-  // remove asked class
-  public static function filterNotClass(array $pieces, $class)
-  {
-    foreach($pieces as $it => $piece) {
-      if ($piece->isClass($class)) {
-        unset($pieces[$it]);
-      }
+    public static function filterType($pieces, $type)
+    {
+        return $this->filterClass($pieces, ucfirst($type));
     }
 
-    return array_values($pieces);
-  }
+    // remove asked class
+    public static function filterNotClass(array $pieces, $class)
+    {
+        foreach($pieces as $it => $piece) {
+            if ($piece->isClass($class)) {
+                unset($pieces[$it]);
+            }
+        }
 
-  public static function filterNotType(array $pieces, $type)
-  {
-      return self::filterClass($pieces, ucfirst($type));
-  }
+        return array_values($pieces);
+    }
 
-  /**
-   * Return pieces that never moved
-   *
-   * @return array of pieces
-   **/
-  public static function filterNotMoved(array $pieces)
-  {
-      foreach ($pieces as $index => $piece) {
-          if($piece->hasMoved()) {
-              unset($pieces[$index]);
-          }
-      }
+    public static function filterNotType(array $pieces, $type)
+    {
+        return self::filterClass($pieces, ucfirst($type));
+    }
 
-      return array_values($pieces);
-  }
+    /**
+     * Return pieces that never moved
+     *
+     * @return array of pieces
+     **/
+    public static function filterNotMoved(array $pieces)
+    {
+        foreach ($pieces as $index => $piece) {
+            if($piece->hasMoved()) {
+                unset($pieces[$index]);
+            }
+        }
+
+        return array_values($pieces);
+    }
 }
