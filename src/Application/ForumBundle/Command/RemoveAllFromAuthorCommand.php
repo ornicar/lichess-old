d7c2127aeb55bf1e3bc3450c36eaf34e8232fa94
ornicar <thibault.duplessis@gmail.com>
2011-03-09 14:09:57 -0800

    Improve forum maintenance command

--- src/Application/ForumBundle/Command/RemoveAllFromAuthorCommand.php
+++ src/Application/ForumBundle/Command/RemoveAllFromAuthorCommand.php
@@ -36,29 +36,55 @@ class RemoveAllFromAuthorCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $authorName = $input->getArgument('authorName');
+        $authorNames = array_map('trim', (array) explode(',', $input->getArgument('authorName')));
 
         $dm = $this->container->get('doctrine.odm.mongodb.document_manager');
         $postRemover = $this->container->get('forum.remover.post');
         $topicRemover = $this->container->get('forum.remover.topic');
 
-        $posts = $this->container->get('forum.repository.post')->findBy(array(
-            'authorName' => $authorName
-        ));
-        $output->writeLn(sprintf('Will remove %d posts from %s', $posts->count(), $authorName));
+        if ($authorNames[0] === 'http') {
+            $posts = $this->container->get('forum.repository.post')->createQueryBuilder()
+                ->field('authorName')->equals(new \MongoRegex('/^http\:\//'))
+                ->getQuery()
+                ->execute();
+            $output->writeLn(sprintf('Will remove %d posts http:', $posts->count()));
 
-        foreach ($posts as $post) {
-            try {
-                $postRemover->remove($post);
-            } catch (\LogicException $e) {
+            foreach ($posts as $post) {
                 try {
-                    $topicRemover->remove($post->getTopic());
-                } catch (\LogicException $e) {
+                    if ($post->getNumber() == 1) {
+                        $topicRemover->remove($post->getTopic());
+                    } else {
+                        $postRemover->remove($post);
+                    }
+                } catch (\Exception $e) {
+                    $output->writeLn($post->getId().' '.$e->getMessage());
+                }
+            }
+            $dm->flush();
+            $output->writeLn('Done');
+            return;
+        }
+
+        foreach ($authorNames as $authorName) {
+            $posts = $this->container->get('forum.repository.post')->findBy(array(
+                'authorName' => $authorName
+            ));
+            $output->writeLn(sprintf('Will remove %d posts from %s', $posts->count(), $authorName));
+
+            foreach ($posts as $post) {
+                try {
+                    if ($post->getNumber() == 1) {
+                        $topicRemover->remove($post->getTopic());
+                    } else {
+                        $postRemover->remove($post);
+                    }
+                } catch (\Exception $e) {
                     $output->writeLn($e->getMessage());
                 }
             }
+            $dm->flush();
+            $dm->clear();
         }
-        $dm->flush();
 
         $output->writeLn('Done');
     }
