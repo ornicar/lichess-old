c8f8e4db206b9979fd50d1ba57e3147f6635dacd
ornicar <thibault.duplessis@gmail.com>
2011-03-11 12:03:09 -0800

    Make online user cache aware of lichess synchronizer timeout

--- src/Application/UserBundle/Online/Cache.php
+++ src/Application/UserBundle/Online/Cache.php
@@ -6,6 +6,13 @@ use Application\UserBundle\Document\User;
 
 class Cache
 {
+    protected $timeout;
+
+    public function __construct($timeout)
+    {
+        $this->timeout = $timeout;
+    }
+
     public function setOnline(User $user)
     {
         return $this->setUsernameOnline($user->getUsername());
@@ -13,7 +20,7 @@ class Cache
 
     public function setUsernameOnline($username)
     {
-        apc_store('online.'.$username, true, $this->getTimeout());
+        apc_store('online.'.$username, true, $this->timeout);
     }
 
     public function isOnline(User $user)
@@ -30,7 +37,7 @@ class Cache
     {
         $it = new \APCIterator('user', '/^online\./', APC_ITER_MTIME | APC_ITER_KEY, 100, APC_LIST_ACTIVE);
         $usernames = array();
-        $limit = time() - $this->getTimeout();
+        $limit = time() - $this->timeout;
         foreach($it as $i) {
             apc_fetch($i['key']); // clear invalidated entries
             if($i['mtime'] >= $limit) {
@@ -39,10 +46,5 @@ class Cache
         }
 
         return $usernames;
-    }
-
-    protected function getTimeout()
-    {
-        return 20;
     }
 }
