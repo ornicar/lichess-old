ab128d36488db8ad64241b5d5ec22b9f9915ebe8
ornicar <thibault.duplessis@gmail.com>
2011-03-11 16:26:39 -0800

    Fix remember_me

--- src/Application/ForumBundle/AuthorNamePersistence.php
+++ src/Application/ForumBundle/AuthorNamePersistence.php
@@ -55,6 +55,6 @@ class AuthorNamePersistence
 
     protected function isAnonymous()
     {
-        return !$this->securityContext->isGranted('IS_AUTHENTICATED_FULLY');
+        return !$this->securityContext->isGranted('ROLE_USER');
     }
 }
