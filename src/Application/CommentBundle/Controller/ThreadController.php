dbf49717d289ddb5a6edb091047b80369ffd1a04
ornicar <thibault.duplessis@gmail.com>
2011-03-17 18:14:36 -0700

    Override comment thread action and template

--- src/Application/CommentBundle/Controller/ThreadController.php
+++ src/Application/CommentBundle/Controller/ThreadController.php
@@ -3,8 +3,8 @@
 namespace Application\CommentBundle\Controller;
 
 use FOS\CommentBundle\Controller\ThreadController as BaseThreadController;
-
 use FOS\CommentBundle\Model\ThreadInterface;
+use Bundle\LichessBundle\Document\Game;
 
 class ThreadController extends BaseThreadController
 {
@@ -14,6 +14,25 @@ class ThreadController extends BaseThreadController
         $this->container->get('lichess_comment.authorname_persistence')->loadComment($comment);
 
         return $comment;
+    }
+
+    public function showGameAction(Game $game)
+    {
+        $identifier = 'game:'.$game->getId();
+        $thread = $this->container->get('fos_comment.manager.thread')->findThreadByIdentifier($identifier);
+        if (!$thread) {
+            $thread = $this->container->get('fos_comment.creator.thread')->create($identifier);
+        }
+
+        $comment = $this->createComment($thread);
+        $form = $this->container->get('fos_comment.form_factory.comment')->createForm();
+        $form->setData($comment);
+
+        return $this->container->get('templating')->renderResponse('LichessCommentBundle:Thread:showGame.html.twig', array(
+            'game'   => $game,
+            'thread' => $thread,
+            'form'   => $form
+        ));
     }
 
     public function showFeedAction($identifier)
