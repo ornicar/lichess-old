5c09788710ec6469767eebfc5801a76826f8f6d9
ornicar <thibault.duplessis@gmail.com>
2011-03-17 22:22:55 -0700

    UI redesign

--- src/Application/CommentBundle/Controller/ThreadController.php
+++ src/Application/CommentBundle/Controller/ThreadController.php
@@ -16,25 +16,6 @@ class ThreadController extends BaseThreadController
         return $comment;
     }
 
-    public function showGameAction(Game $game)
-    {
-        $identifier = 'game:'.$game->getId();
-        $thread = $this->container->get('fos_comment.manager.thread')->findThreadByIdentifier($identifier);
-        if (!$thread) {
-            $thread = $this->container->get('fos_comment.creator.thread')->create($identifier);
-        }
-
-        $comment = $this->createComment($thread);
-        $form = $this->container->get('fos_comment.form_factory.comment')->createForm();
-        $form->setData($comment);
-
-        return $this->container->get('templating')->renderResponse('LichessCommentBundle:Thread:showGame.html.twig', array(
-            'game'   => $game,
-            'thread' => $thread,
-            'form'   => $form
-        ));
-    }
-
     public function showFeedAction($identifier)
     {
         return parent::showFeedAction('game:'.$identifier);
