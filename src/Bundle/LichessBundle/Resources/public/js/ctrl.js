8e25d1e9482d7939691f5dc2e826d7b0018db2ee
ornicar <thibault.duplessis@gmail.com>
2011-03-17 18:17:50 -0700

    Improve game templates

--- src/Bundle/LichessBundle/Resources/public/js/ctrl.js
+++ src/Bundle/LichessBundle/Resources/public/js/ctrl.js
@@ -173,6 +173,8 @@ $(function() {
 		setTimeout(function() {
 			// share links
 			$('ul.lichess_social').html('<li class="lichess_stumbleupon"><iframe src="http://www.stumbleupon.com/badge/embed/2/?url=http://lichess.org/"></iframe></li><li class="lichess_facebook"><iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Flichess.org%2F&amp;layout=button_count&amp;show_faces=false&amp;width=110&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=22"></iframe></li>');
+            $('div.game_sharing').append('<iframe src="http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(document.url)+"&amp;layout=button_count&amp;show_faces=false&amp;width=110&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=22"></iframe>');
+            $.getScript('http://platform.twitter.com/widgets.js');
 		},
 		800);
 	}
