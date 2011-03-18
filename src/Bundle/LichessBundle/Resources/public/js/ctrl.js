0534f4ae13594a9cdd1d5ad0c5d66a96ca1276d4
ornicar <thibault.duplessis@gmail.com>
2011-03-17 19:44:26 -0700

    Tweak share links

--- src/Bundle/LichessBundle/Resources/public/js/ctrl.js
+++ src/Bundle/LichessBundle/Resources/public/js/ctrl.js
@@ -169,12 +169,14 @@ $(function() {
 		}
 	}
 
-	if (document.domain == 'lichess.org') {
+	if (false || document.domain == 'lichess.org') {
 		setTimeout(function() {
 			// share links
 			$('ul.lichess_social').html('<li class="lichess_stumbleupon"><iframe src="http://www.stumbleupon.com/badge/embed/2/?url=http://lichess.org/"></iframe></li><li class="lichess_facebook"><iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Flichess.org%2F&amp;layout=button_count&amp;show_faces=false&amp;width=110&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=22"></iframe></li>');
-            $('div.game_sharing').append('<iframe src="http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(document.url)+"&amp;layout=button_count&amp;show_faces=false&amp;width=110&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=22"></iframe>');
-            $.getScript('http://platform.twitter.com/widgets.js');
+            if ($gameSharing = $('div.game_sharing').orNot()) {
+                $gameSharing.prepend('<div class="lichess_facebook"><iframe src="http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(document.url)+'&amp;layout=button_count&amp;show_faces=false&amp;width=110&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=22"></iframe></div>');
+                $.getScript('http://platform.twitter.com/widgets.js', function() { $gameSharing.addClass('loaded') });
+            }
 		},
 		800);
 	}
