639cc168f2497310d59e15d38b18e1606ed2a3dc
ornicar <thibault.duplessis@gmail.com>
2011-03-18 21:01:16 -0700

    Implement timeline filters

--- src/Bundle/LichessBundle/Resources/public/js/timeline.js
+++ src/Bundle/LichessBundle/Resources/public/js/timeline.js
@@ -1,15 +1,25 @@
 $(function() {
-	if ($timeline = $('.timeline_entries').orNot()) {
-		refreshUrl = $timeline.attr('data-url');
-		(function refresh() {
-			setTimeout(function() {
-				$.get(refreshUrl, function(html) {
-					$timeline.html(html);
-					refresh();
-				});
-			},
-			2500);
-		})();
-	}
+	$timeline = $('.timeline_entries');
+    refreshUrl = $timeline.attr('data-url');
+    $filters = $filters = $('div.timeline_filters a');
+    var activeFilter = $filters.filter('.active').attr('data-filter');
+    (function refresh() {
+        setTimeout(function() {
+            $.get(refreshUrl, function(html) {
+                $timeline.html(html);
+                filterEntries();
+                refresh();
+            });
+        },
+        2500);
+    })();
+    $filters.click(function() {
+        $filters.removeClass('active');
+        $(this).addClass('active');
+        activeFilter = $(this).attr('data-filter');
+        filterEntries();
+    });
+    filterEntries = function() {
+        $timeline.find('> li').show().not('.'+activeFilter).hide();
+    }
 });
-
