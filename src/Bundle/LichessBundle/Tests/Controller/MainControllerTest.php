7479b52c4594a0ee98b3d4226cb0c5b00655a929
ornicar <thibault.duplessis@gmail.com>
2011-03-18 20:39:02 -0700

    Remove translations tests

--- src/Bundle/LichessBundle/Tests/Controller/MainControllerTest.php
+++ src/Bundle/LichessBundle/Tests/Controller/MainControllerTest.php
@@ -30,38 +30,4 @@ class MainControllerTest extends WebTestCase
         $this->assertRegexp('/^\d+$/', (string)$client->getResponse()->getContent());
     }
 
-    /**
-     * @dataProvider getCultures
-     *
-     * @param mixed $culture
-     */
-    public function testTranslation($culture, $gameUrl)
-    {
-        $client = $this->createClient();
-        $client->request('GET', '/locale/'.$culture);
-        $this->assertTrue($client->getResponse()->isRedirect());
-        $crawler = $client->followRedirect();
-        $this->assertTrue($client->getResponse()->isSuccessful());
-        $this->assertEquals($culture, $crawler->filter('html')->attr('lang'));
-        $crawler = $client->request('GET', $gameUrl);
-        $this->assertTrue($client->getResponse()->isSuccessful());
-        $this->assertEquals($culture, $crawler->filter('html')->attr('lang'));
-    }
-
-    public function getCultures()
-    {
-        $container = $this->createClient()->getContainer();
-        $locales = $container->getParameter('lichess.locales');
-        $game = $container->get('lichess.repository.game')->findOneBy(array());
-        $gameUrl = '/'.$game->getId();
-
-        $cultures = array();
-        foreach ($locales as $locale => $name) {
-            $cultures[] = array($locale, $gameUrl);
-        }
-        $cultures[] = array('en', $gameUrl);
-
-        return $cultures;
-    }
-
 }
