ccc588655c4b41ea3d9a73eed67e874938ba6c91
ornicar <thibault.duplessis@gmail.com>
2011-03-29 13:23:01 -0700

    Add functional tests for translations

--- src/Lichess/TranslationBundle/Tests/Acceptance/TranslationAcceptanceTest.php
+++ src/Lichess/TranslationBundle/Tests/Acceptance/TranslationAcceptanceTest.php
@@ -3,9 +3,33 @@
 namespace Lichess\TranslationBundle\Tests\Acceptance;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
 
 class TranslationControllerTest extends WebTestCase
 {
+    public function testRequestWithTranslatedLanguage()
+    {
+        $client = $this->createPersistentClient();
+        $server = array(
+            'HTTP_ACCEPT_LANGUAGE' => 'fr'
+        );
+        $crawler = $client->request('GET', '/', array(), array(), $server);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(0, $crawler->filter('#incomplete_translation')->count());
+        $this->assertRegexp('/Jouer avec un ami/', $client->getResponse()->getContent());
+    }
+
+    public function testRequestWithNonTranslatedLanguage()
+    {
+        $client = $this->createPersistentClient();
+        $server = array(
+            'HTTP_ACCEPT_LANGUAGE' => 'kg'
+        );
+        $crawler = $client->request('GET', '/', array(), array(), $server);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('#incomplete_translation')->count());
+    }
+
     /**
      * @dataProvider translationProvider
      */
@@ -34,5 +58,14 @@ class TranslationControllerTest extends WebTestCase
         }
 
         return $data;
+    }
+
+    protected function createPersistentClient($cookieName = 'test')
+    {
+        $client = parent::createClient();
+        $client->getContainer()->get('session.storage.file')->deleteFile();
+        $client->getCookieJar()->set(new Cookie(session_name(), $cookieName));
+
+        return $client;
     }
 }
