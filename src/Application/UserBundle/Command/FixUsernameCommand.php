e0996cb983af8bccc1ba43483a5cae4902f94a8f
ornicar <thibault.duplessis@gmail.com>
2011-06-14 21:33:09 -0700

    Add a command to fix invalid usernames

---
+++ src/Application/UserBundle/Command/FixUsernameCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Application\UserBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class FixUsernameCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setDefinition(array())
+            ->addOption('flush', null, InputOption::VALUE_NONE, 'Flushes the changes to the DB')
+            ->setName('lichess:fix-usernames')
+            ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $objectManager = $this->container->get('doctrine.odm.mongodb.document_manager');
+        $userRepo = $this->container->get('fos_user.repository.user');
+
+        $users = $userRepo->findAll();
+
+        foreach($users as $user) {
+            if(preg_match('/[^\w]/', $user->getUsername())) {
+                $newUsername = $this->fix($user->getUsername());
+                if ($newUsername === $user->getUsername()) {
+                    continue;
+                }
+                $newUsernameCanonical = $this->fix($user->getUsernameCanonical());
+                $output->writeLn(sprintf('Modified %s to %s', $user->getUsername(), $newUsername));
+                $user->setUsername($newUsername);
+                $user->setUsernameCanonical($newUsernameCanonical);
+            }
+        }
+
+        if ($input->getOption('flush')) {
+            $output->writeln('Flushing');
+            $objectManager->flush();
+        } else {
+            $output->writeln('NOT flushing');
+        }
+    }
+
+    protected function fix($text)
+    {
+        $text = str_replace('-', 'arstdhneioqwfpgjluy', $text);
+        $text = preg_replace('/[^A-Za-z0-9_]/', '-', $text);
+        $text = str_replace('arstdhneioqwfpgjluy', '-', $text);
+
+        return $text;
+    }
+}
