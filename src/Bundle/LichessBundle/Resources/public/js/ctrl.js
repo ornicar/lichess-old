f51b8b19ca0e1222487dbb9d220afb155943ae23
ornicar <thibault.duplessis@gmail.com>
2011-06-25 19:18:52 -0700

    Improve share links

--- src/Bundle/LichessBundle/Resources/public/js/ctrl.js
+++ src/Bundle/LichessBundle/Resources/public/js/ctrl.js
@@ -192,7 +192,6 @@ $(function() {
         setTimeout(function() {
             if ($gameSharing = $('div.game_share_widgets').orNot()) {
                 $gameSharing.find('div.facebook_placeholder').replaceWith('<div class="lichess_facebook"><iframe src="http://www.facebook.com/plugins/like.php?href=' + encodeURIComponent(document.location.href) + '&amp;layout=button_count&amp;show_faces=false&amp;width=110&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=22"></iframe></div>');
-                $.getScript('http://widgets.digg.com/buttons.js');
                 $.getScript('http://platform.twitter.com/widgets.js', function() {
                     $gameSharing.addClass('loaded')
                 });
