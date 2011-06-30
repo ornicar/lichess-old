dfdb1825c39687d97b677e606370c5f9485f5264
ornicar <thibault.duplessis@gmail.com>
2011-06-30 15:39:11 -0700

    Use apc class loader in xhr autoloader

--- xhr/autoload.php
+++ xhr/autoload.php
@@ -1,18 +1,19 @@
 <?php
 
-$vendorDir = realpath(__DIR__.'/../vendor');
-$srcDir = realpath(__DIR__.'/../src');
+$vendorDir  = realpath(__DIR__.'/../vendor');
+$srcDir     = realpath(__DIR__.'/../src');
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
+require $vendorDir.'/symfony/src/Symfony/Component/ClassLoader/ApcUniversalClassLoader.php';
 
-$loader = new UniversalClassLoader();
+$loader = new Symfony\Component\ClassLoader\ApcUniversalClassLoader('lichess.cl.');
+
 $loader->registerNamespaces(array(
-    'Symfony'                        => array($vendorDir.'/symfony/src', $srcDir),
-    'Doctrine\\MongoDB'              => $vendorDir.'/doctrine-mongodb/lib',
-    'Doctrine\\ODM\\MongoDB'         => $vendorDir.'/doctrine-mongodb-odm/lib',
-    'Doctrine\\Common'               => $vendorDir.'/doctrine-common/lib',
-    'Bundle'                         => $srcDir,
-    'FOS'                            => $srcDir,
-    'Application'                    => $srcDir
+    'Symfony'                => array($vendorDir.'/symfony/src', $srcDir),
+    'Doctrine\\MongoDB'      => $vendorDir.'/doctrine-mongodb/lib',
+    'Doctrine\\ODM\\MongoDB' => $vendorDir.'/doctrine-mongodb-odm/lib',
+    'Doctrine\\Common'       => $vendorDir.'/doctrine-common/lib',
+    'Bundle'                 => $srcDir,
+    'FOS'                    => $srcDir,
+    'Application'            => $srcDir
 ));
 $loader->register();
