81daf66318a56bba15e3edf4d511d9df12a9edb2
ornicar <thibault.duplessis@gmail.com>
2011-07-06 20:59:09 -0700

    Remove old code

--- src/Bundle/LichessBundle/Tests/Controller/AbstractControllerTest.php
+++ src/Bundle/LichessBundle/Tests/Controller/AbstractControllerTest.php
@@ -25,33 +25,6 @@ abstract class AbstractControllerTest extends WebTestCase
         return array($client, $crawler);
     }
 
-    protected function inviteAnybody($join = false)
-    {
-        $client = self::createClient();
-        !$join && $this->clearSeekQueue($client);
-        $crawler = $client->request('GET', '/');
-        $crawler = $client->click($crawler->selectLink('Play with anybody')->link());
-        $this->assertTrue($client->getResponse()->isSuccessful());
-        $form = $crawler->filter('.submit')->form();
-        $client->submit($form);
-        $this->assertTrue($client->getResponse()->isRedirect());
-        $crawler = $client->followRedirect();
-        $this->assertTrue($client->getResponse()->isSuccessful());
-        if($join) {
-            $redirectUrl = $crawler->filter('a.join_redirect_url')->attr('href');
-            $client->request('GET', $redirectUrl);
-            $this->assertTrue($client->getResponse()->isRedirect());
-            $crawler = $client->followRedirect();
-            $this->assertTrue($client->getResponse()->isSuccessful());
-            $this->assertEquals(1, $crawler->filter('div.lichess_game.lichess_player_black')->count());
-        } else {
-            $this->assertRegexp('/Waiting for opponent/', $client->getResponse()->getContent());
-            $this->assertEquals(1, $crawler->filter('div.lichess_game_not_started.lichess_player_white')->count());
-        }
-
-        return array($client, $crawler);
-    }
-
     protected function clearSeekQueue($client)
     {
         $client->getContainer()->get('lichess.repository.seek')->createQueryBuilder()->remove()->getQuery()->execute();
