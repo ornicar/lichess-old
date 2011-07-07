3a5339a5aea8202e49f0764361bad469bd58dcca
ornicar <thibault.duplessis@gmail.com>
2011-07-07 16:30:14 -0700

    Increase hook sync latency when window is blured

--- src/Lichess/OpeningBundle/Resources/public/js/hook.js
+++ src/Lichess/OpeningBundle/Resources/public/js/hook.js
@@ -3,11 +3,15 @@ $(function() {
     var $wrap = $('div.hooks_wrap');
     var $hooks = $wrap.find('div.hooks');
     var url = $hooks.data('url');
+    var slowDelay = 3000, fastDelay = delay = 500;
+    var timeout;
 
-    (function reload() {
-        setTimeout(function() {
+    function reload() {
+        timeout = setTimeout(function() {
+            timeout = false;
             $.ajax(url, {
                 success: function(html) {
+                    // redirections
                     if (/^\//.test(html)) {
                         location.href = html;
                         return;
@@ -24,13 +28,24 @@ $(function() {
                 timeout: 9000
             });
         },
-        700);
-    })();
+        delay);
+    };
+    reload();
 
     $wrap.delegate('tr.joinable', 'click', function() {
         location.href = $(this).find('a.join').attr('href');
     });
     $wrap.delegate('tr.empty', 'click', function() {
         $('#start_buttons a.config_hook').click();
+    });
+
+    $(window).bind('blur', function() {
+        delay = slowDelay;
+    }).bind('focus', function() {
+        delay = fastDelay;
+        if (timeout) {
+            clearTimeout(timeout);
+            reload();
+        }
     });
 });
