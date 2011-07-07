ce80b1a04a634ef520152f13683462c6b894e0fe
ornicar <thibault.duplessis@gmail.com>
2011-07-07 10:33:50 -0700

    Various fixes

--- src/Application/ForumBundle/Controller/SearchController.php
+++ src/Application/ForumBundle/Controller/SearchController.php
@@ -11,6 +11,8 @@ class SearchController extends Controller
         $query = $this->get('request')->query->get('q', '');
         if ($query) {
             $results = $this->get('foq_elastica.finder.default.forum')->findPaginated($query);
+            $results->setMaxPerPage(15);
+            $results->setCurrentPage($this->get('request')->query->get('page', 1));
 
             return $this->render('LichessForumBundle:Search:results.html.twig', compact('query', 'results'));
         }
