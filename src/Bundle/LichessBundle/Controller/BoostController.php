371be60db2ce1c62ddfc7791eb0e431fa03ec80f
ornicar <thibault.duplessis@gmail.com>
2011-07-13 20:53:30 -0700

    Fix boost controller

--- src/Bundle/LichessBundle/Controller/BoostController.php
+++ src/Bundle/LichessBundle/Controller/BoostController.php
@@ -4,6 +4,7 @@ namespace Bundle\LichessBundle\Controller;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Bundle\LichessBundle\Boost\Handler;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Should not be hit unless you disabled the boost router
@@ -12,11 +13,11 @@ class BoostController extends ContainerAware
 {
     public function howManyPlayersNowAction()
     {
-        return new Response((string) Handler::howManyPlayersNowAction(), 'text/plain');
+        return new Response((string) Handler::howManyPlayersNowAction(), 200, array('content-type' => 'text/plain'));
     }
 
     public function pingAction()
     {
-        return new Response((string) Handler::ping(), 'application/json');
+        return new Response((string) Handler::ping(), 200, array('content-type' => 'application/json'));
     }
 }
