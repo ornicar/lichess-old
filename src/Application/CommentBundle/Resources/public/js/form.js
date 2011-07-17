17f26270510887f239c4f4ed9db2646840a2431a
ornicar <thibault.duplessis@gmail.com>
2011-07-17 14:49:33 -0700

    Improve game commenting clientside performance

--- src/Application/CommentBundle/Resources/public/js/form.js
+++ src/Application/CommentBundle/Resources/public/js/form.js
@@ -1,26 +1,27 @@
-$('form.fos_comment_comment_form').live('submit', function() {
-    var $form = $(this).addClass('processing').ajaxSubmit({
-        success: function(html) {
-            $form.closest('div.fos_comment_thread_show').replaceWith(html);
-        },
-        error: function(xhr, status, error) {
-            $form.addClass('error').removeClass('processing');
-        }
+$(function() {
+    $('#lichess div.game_share').delegate('form.fos_comment_comment_form', 'submit', function() {
+        var $form = $(this).addClass('processing').ajaxSubmit({
+            success: function(html) {
+                $form.closest('div.fos_comment_thread_show').replaceWith(html);
+            },
+            error: function(xhr, status, error) {
+                $form.addClass('error').removeClass('processing');
+            }
+        });
+        return false;
+    }).delegate('button.fos_comment_comment_reply_show_form', 'click', function() {
+        var $button = $(this);
+        var $container = $button.parent().addClass('replying');
+        var $reply = $('div.fos_comment_reply_prototype').clone()
+            .removeClass('fos_comment_reply_prototype')
+            .find('.fos_comment_reply_name_placeholder').text($button.attr('data-name')).end()
+            .find('.fos_comment_reply_cancel').click(function() {
+                $reply.remove();
+                $container.removeClass('replying');
+            }).end()
+            .appendTo($container)
+            .find('textarea').focus().end();
+        // add the comment parent id to the form action url
+        $reply.find('form').attr('action', $reply.find('form').attr('action')+'/'+$button.attr('data-id'));
     });
-    return false;
-});
-$('button.fos_comment_comment_reply_show_form').live('click', function() {
-    var $button = $(this);
-    var $container = $button.parent().addClass('replying');
-    var $reply = $('div.fos_comment_reply_prototype').clone()
-        .removeClass('fos_comment_reply_prototype')
-        .find('.fos_comment_reply_name_placeholder').text($button.attr('data-name')).end()
-        .find('.fos_comment_reply_cancel').click(function() {
-            $reply.remove();
-            $container.removeClass('replying');
-        }).end()
-        .appendTo($container)
-        .find('textarea').focus().end();
-    // add the comment parent id to the form action url
-    $reply.find('form').attr('action', $reply.find('form').attr('action')+'/'+$button.attr('data-id'));
 });
