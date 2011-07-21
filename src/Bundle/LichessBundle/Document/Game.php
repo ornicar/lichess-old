35c1aeef16bc1eead6c78707ee5a3cfdc9802753
ornicar <thibault.duplessis@gmail.com>
2011-07-20 17:49:40 -0700

    Denormalize Game.isAi

--- src/Bundle/LichessBundle/Document/Game.php
+++ src/Bundle/LichessBundle/Document/Game.php
@@ -222,6 +222,15 @@ class Game
     protected $next;
 
     /**
+     * True if this is a game played with an AI
+     *
+     * @var boolean
+     * @MongoDB\Boolean
+     * @MongoDB\Index
+     */
+    protected $isAi;
+
+    /**
      * Config values used to create the game. Cleared when game starts.
      *
      * @var array
@@ -775,6 +784,7 @@ class Game
         if($this->getIsRated()) {
             $this->addRoomMessage('system', 'This game is rated');
         }
+        $this->isAi = $this->getInvited()->getIsAi();
         $this->setConfigArray(null);
     }
 
