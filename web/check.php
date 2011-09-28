64fe5cfb79755165cfcf7c764739a98fc0798b15
ornicar <thibault.duplessis@gmail.com>
2011-09-28 23:18:52 +0200

    Update check.php for 2 applications

--- web/check.php
+++ web/check.php
@@ -47,8 +47,10 @@ EOF
 echo_title("Mandatory requirements");
 check(version_compare(phpversion(), '5.3.1', '>='), sprintf('Checking that PHP version is at least 5.3.1 (%s installed)', phpversion()), 'Install PHP 5.3.1 or newer (current version is '.phpversion(), true);
 check(ini_get('date.timezone'), 'Checking that the "date.timezone" setting is set', 'Set the "date.timezone" setting in php.ini (like Europe/Paris)', true);
-check(is_writable(__DIR__.'/../lichess/cache'), sprintf('Checking that cache/ directory is writable'), 'Change the permissions of the lichess/cache/ directory so that the web server can write in it', true);
-check(is_writable(__DIR__.'/../lichess/logs'), sprintf('Checking that the logs/ directory is writable'), 'Change the permissions of the lichess/logs/ directory so that the web server can write in it', true);
+check(is_writable(__DIR__.'/../app/cache'), sprintf('Checking that app/cache/ directory is writable'), 'Change the permissions of the app/cache/ directory so that the web server can write in it', true);
+check(is_writable(__DIR__.'/../app/logs'), sprintf('Checking that the app/logs/ directory is writable'), 'Change the permissions of the app/logs/ directory so that the web server can write in it', true);
+check(is_writable(__DIR__.'/../xhr/cache'), sprintf('Checking that xhr/cache/ directory is writable'), 'Change the permissions of the xhr/cache/ directory so that the web server can write in it', true);
+check(is_writable(__DIR__.'/../xhr/logs'), sprintf('Checking that the xhr/logs/ directory is writable'), 'Change the permissions of the xhr/logs/ directory so that the web server can write in it', true);
 
 $accelerator = (function_exists('apc_store') && ini_get('apc.enabled'));
 check($accelerator, 'Checking that APC is installed', 'Install APC', true);
