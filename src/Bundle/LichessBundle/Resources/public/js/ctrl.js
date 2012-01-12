4a22717df4b2714136bda896d8ed58cd68b0b806
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-12 20:25:10 +0100

    Reduce ping frequency

--- src/Bundle/LichessBundle/Resources/public/js/ctrl.js
+++ src/Bundle/LichessBundle/Resources/public/js/ctrl.js
@@ -5,7 +5,7 @@ if (typeof console == "undefined" || typeof console.log == "undefined") console 
 $(function() {
 
     // Start ping
-    var pingDelay = 2500;
+    var pingDelay = 3000;
     var connectivity = new $.connectivity($('#connectivity'), {
         delay: pingDelay,
         tolerance: 300
