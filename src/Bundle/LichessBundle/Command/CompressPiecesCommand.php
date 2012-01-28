4d9e3d842d3a57b106a926c021c193d3f37ffd44
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-28 17:34:24 +0100

    Improve migration command

--- src/Bundle/LichessBundle/Command/CompressPiecesCommand.php
+++ src/Bundle/LichessBundle/Command/CompressPiecesCommand.php
@@ -40,59 +40,59 @@ class CompressPiecesCommand extends ContainerAwareCommand
 
         $it = 0;
 
-        $cursor = $collection->find(array('players.moveTimes' => array('$exists' => 1), 'players.pieces' => array('$exists' => 1)), array('pgnMoves' => true, 'players' => true));
+        $cursor = $collection->find(array(), array('pgnMoves' => true, 'players.pieces' => true, 'players.moveTimes' => true));
 
         while($game = $cursor->getNext()) {
             $id = (string) $game['_id'];
             $playerPieces = array();
             $playerMts = array();
-            foreach ($game['players'] as $pi => $player) {
-                if (empty($player['pieces'])) {
-                    var_dump($id);
-                    break;
+            if (count($game['players']) != 2) {
+                $output->writeLn("Game $id has " . count($game['players']) . " players, removing");
+                $collection->remove(array('_id' => $id), array('safe' => true));
+            } else {
+                foreach ($game['players'] as $pi => $player) {
+                    if (empty($player['pieces']) || !is_array($player['pieces'])) {
+                        $output->writeLn("Game $id has a player without pieces");
+                        break;
+                    }
+                    $ps = array();
+                    foreach($player['pieces'] as $piece) {
+                        $letter = $piece['t'];
+                        if (isset($piece['isDead']) && $piece['isDead']) $letter = strtoupper($letter);
+                        $fm = isset($piece['firstMove']) ? $piece['firstMove'] : "";
+                        $ps[] = Board::keyToPiotr(Board::posToKey($piece['x'], $piece['y'])) . $letter . $fm;
+                    }
+                    $playerPieces[$pi] = implode(' ', $ps);
+                    if (!empty($player['moveTimes'])) {
+                        $playerMts[$pi] = implode(' ', $player['moveTimes']);
+                    }
                 }
-                if (!is_array($player['pieces'])) {
-                    var_dump($id);
-                    var_dump($player['pieces']);
-                    break;
+                if (count($playerPieces) == 2) {
+                    $update = array(
+                        '$unset' => array(
+                            'players.0.pieces' => true,
+                            'players.1.pieces' => true,
+                            'players.0.stack' => true,
+                            'players.1.stack' => true,
+                            'players.0.moveTimes' => true,
+                            'players.1.moveTimes' => true,
+                            'positionHashes' => true,
+                            'pgnMoves' => true
+                        ),
+                        '$set' => array(
+                            'players.0.ps' => $playerPieces[0],
+                            'players.1.ps' => $playerPieces[1],
+                            'pgn' => isset($game['pgnMoves']) ? implode(' ', $game['pgnMoves']) : null
+                        )
+                    );
+                    foreach($playerMts as $pmi => $mts) {
+                        $update['$set']["players.$pmi.mts"] = $mts;
+                    }
+                    $collection->update(array('_id' => $id), $update, array('multiple' => false, 'safe' => true));
                 }
-                $ps = array();
-                foreach($player['pieces'] as $piece) {
-                    $letter = $piece['t'];
-                    if (isset($piece['isDead']) && $piece['isDead']) $letter = strtoupper($letter);
-                    $fm = isset($piece['firstMove']) ? $piece['firstMove'] : "";
-                    $ps[] = Board::keyToPiotr(Board::posToKey($piece['x'], $piece['y'])) . $letter . $fm;
-                }
-                $playerPieces[$pi] = implode(' ', $ps);
-                if (!empty($player['moveTimes'])) {
-                    $playerMts[$pi] = implode(' ', $player['moveTimes']);
-                }
-            }
-            if (count($playerPieces) == 2) {
-                $update = array(
-                    '$unset' => array(
-                        'players.0.pieces' => true,
-                        'players.1.pieces' => true,
-                        'players.0.stack' => true,
-                        'players.1.stack' => true,
-                        'players.0.moveTimes' => true,
-                        'players.1.moveTimes' => true,
-                        'positionHashes' => true,
-                        'pgnMoves' => true
-                    ),
-                    '$set' => array(
-                        'players.0.ps' => $playerPieces[0],
-                        'players.1.ps' => $playerPieces[1],
-                        'pgn' => isset($game['pgnMoves']) ? implode(' ', $game['pgnMoves']) : null
-                    )
-                );
-                foreach($playerMts as $pmi => $mts) {
-                    $update['$set']["players.$pmi.mts"] = $mts;
-                }
-                $collection->update(array('_id' => $id), $update, array('multiple' => false, 'safe' => true));
             }
 
-            if (0 == (++$it) % 1000) $output->writeLn($it);
+            if (0 == (++$it) % 10000) $output->writeLn($it);
         }
         $output->writeLn('Done');
     }
