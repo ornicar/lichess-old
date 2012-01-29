b2a5729710fdbe9ca59401b6ad5076a509317258
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-29 23:43:56 +0100

    Ridiculous micro-optimization

--- src/Bundle/LichessBundle/Document/Player.php
+++ src/Bundle/LichessBundle/Document/Player.php
@@ -651,8 +651,9 @@ class Player
     {
         $pieces = array();
         if (!empty($this->ps)) {
+            $baseClass = 'Bundle\\LichessBundle\\Document\\Piece\\';
             foreach(explode(' ', $this->ps) as $p) {
-                $class = 'Bundle\\LichessBundle\\Document\\Piece\\'.Piece::letterToClass(strtolower($p{1}));
+                $class = $baseClass . Piece::letterToClass(strtolower($p{1}));
                 $pos = Board::keyToPos(Board::piotrToKey($p{0}));
                 $piece = new $class($pos[0], $pos[1]);
                 if (ctype_upper($p{1})) $piece->setIsDead(true);
