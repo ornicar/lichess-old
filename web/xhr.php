edaaae06a2f6c07f83eb61747019ed352f946a65
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-29 19:55:56 +0100

    Handle xhr not found

--- web/xhr.php
+++ web/xhr.php
@@ -11,4 +11,12 @@ use Symfony\Component\HttpFoundation\Request;
 // Run application
 $kernel = new XhrKernel('prod', false);
 $kernel->loadClassCache();
-$kernel->handle(Request::createFromGlobals())->send();
+try {
+    $kernel->handle(Request::createFromGlobals())->send();
+} catch (\Exception $e) {
+    // All exceptions are catched but the ResourceNotFound one
+    // Make it a 404
+    header(sprintf('HTTP/1.0 404 Not Found'));
+    echo "Not Found.";
+    exit();
+}
