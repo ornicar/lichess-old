460e61bb306071d1c8bf62b00b418c51d1faa9dc
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-05 20:00:39 +0100

    Remove todolist page and improve wiki style

--- src/Bundle/LichessBundle/Controller/MainController.php
+++ src/Bundle/LichessBundle/Controller/MainController.php
@@ -30,15 +30,6 @@ class MainController extends Controller
       );
     }
 
-    public function todolistAction()
-    {
-        $text = file_get_contents($this->container->getParameter('kernel.root_dir').'/../TODO');
-        $items = explode("\n", preg_replace('/^\*\s/m', '', $text));
-        array_pop($items);
-
-        return $this->render('LichessBundle:Main:todolist.html.twig', array('items' => $items));
-    }
-
     public function toggleSoundAction()
     {
         $session = $this->get('session');
