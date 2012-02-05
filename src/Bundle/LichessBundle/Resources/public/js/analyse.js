e263e5f4029950616d63d5ae0ffd2d41c226eb34
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-05 13:18:39 +0100

    Disable analyzis autoplay

--- src/Bundle/LichessBundle/Resources/public/js/analyse.js
+++ src/Bundle/LichessBundle/Resources/public/js/analyse.js
@@ -1,11 +1,6 @@
 jQuery(function() {
 	SetImagePath("/vendor/pgn4web/lichess/64"); // use "" path if images are in the same folder as this javascript file
 	SetImageType("png");
-	setTimeout(function() {
-		SetAutoplayDelay(2000);
-		SetAutoPlay(true);
-	},
-	1000);
 });
 
 function customFunctionOnPgnGameLoad() {
