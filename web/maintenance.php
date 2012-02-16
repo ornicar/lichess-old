8bfb7653178e8fd1936099c8d58a37e6ce300412
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-17 00:55:39 +0100

    Improve maintenance page

--- web/maintenance.php
+++ web/maintenance.php
@@ -4,6 +4,27 @@ sleep(2); // Avoid rush
 
 header(sprintf('HTTP/1.0 503 Service Unavailable'));
 
-echo "<h1>lichess.org</h1><h2>Temporarily down for maintenance</h2>We expect to be back in a few seconds. Thanks for your patience.";
-
+?>
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>lichess - maintenance</title>
+      <style type="text/css">
+html, body {
+  font:16px monospace;
+  background: #000;
+  color: #ebd488;
+  text-align: center;
+}
+      </style>
+    </head>
+    <body>
+      <h1>lichess.org is down for maintenance</h2>We expect to be back in a few seconds. Thanks for your patience.
+<br />
+<br />
+      <img src="/images/maintenance.jpg" alt="Lichess maintenance" />
+    </body>
+  </html>
+<?php
 exit();
