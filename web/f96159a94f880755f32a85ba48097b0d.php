10907f9051029198865c381e2145bf35121f99cc
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-01 11:54:07 +0200

    Remove uneeded web files

--- web/f96159a94f880755f32a85ba48097b0d.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if(false)   apc_clear_cache('user');
-if(true) apc_clear_cache('opcode');
-
-die(json_encode(array('success' => true, 'message' => sprintf('Clear APC user:false, opcode:true'))));
