bcac219d7fedfeedef22d1e00bd1818b666cbb3f
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-24 14:27:49 +0200

    Redirect urls containing /index.php

--- web/index.php
+++ web/index.php
@@ -2,6 +2,14 @@
 
 //include "maintenance.php";
 
+// Redirect /index.php to /
+if(0 === strncmp($_SERVER['REQUEST_URI'], '/index.php', 10)) {
+    $url = substr($_SERVER['REQUEST_URI'], 10);
+    header('Location: '.$url, true, 301);
+    printf('<html><head><meta http-equiv="refresh" content="1;url=%s"/></head></html>', htmlspecialchars($url, ENT_QUOTES));
+    die;
+}
+
 // Symfony2 boot
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
